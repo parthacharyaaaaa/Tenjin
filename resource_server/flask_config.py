25411949a25e12b678cc885503038921a050338b
Parth Acharya <1008parth@gmail.com>
2025-02-11 21:26:35 +0530

    Adds config file for Flask app

---
+++ resource_server/flask_config.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+
+bLoaded : bool = load_dotenv(".env", verbose=True, override=True)
+if not bLoaded:
+    print(f"Failed to load .env file for resource server, please ensure .env exists within this directory: {os.path.dirname(__file__)}")
+    raise FileNotFoundError
+
+class FlaskConfig:
+    try:
+        ### Flask Configurations ###
+        APP_PORT = int(os.environ["FLASK_PORT"])
+        APP_HOST = os.environ["FLASK_HOST"]
+        APP_DEBUG = bool(int(os.environ.get("FLASK_DEBUG", 0)))
+        SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
+
+        # TODO: Add JWT signing key, peer server metadata, and Redis data. (Maybe session configs too if we use a hybrid approach instead of pure REST?)
+
+        ### Database Configurations ###
+        SQLALCHEMY_DATABASE_URI : str = "postgres+psycopg2://{username}:{password}@{host}:{post}/{database}".format(username=os.environ["POSTGRES_USERNAME"],
+                                                                                                                    password=os.environ["POSTGRES_PASSWORD"],
+                                                                                                                    host=os.environ["POSTGRES_HOST"],
+                                                                                                                    port=os.environ.get("POSTGRES_PORT", 5432),
+                                                                                                                    database=os.environ["POSTGRES_DATABASE"])
+        SQLALCHEMY_POOL_SIZE = int(os.environ.get("SQLALCHEMY_POOL_SIZE", 10))
+        SQLALCHEMY_MAX_OVERFLOW = int(os.environ.get("SQLALCHEMY_MAX_OVERFLOW", 5))
+        SQLALCHEMY_POOL_RECYCLE = int(os.environ.get("SQLALCHEMY_POOL_RECYCLE", 600))
+        SQLALCHEMY_POOL_TIMEOUT = int(os.environ.get("SQLALCHEMY_POOL_TIMEOUT", 30))
+        SQLALCHEMY_TRACK_MODIFICATIONS = bool(int(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", 0)))
+
+        GENERIC_HTTP_MESSAGES : dict = {2 : "Success",
+                                3 : "Redirection",
+                                4 : "Client-Side Error",
+                                5 : "There seems to be an issue at our server. Please try again later"}
+
+    except (TypeError, ValueError):
+        print("\n\nERROR: Invalid format/type for environment variables\n\n")
+        raise Exception()
+    except KeyError:
+        print("\n\n ERROR: Failed to load environment variables\n\n")
+        raise KeyError()
+
+
+FLASK_CONFIG_OBJECT = FlaskConfig()
