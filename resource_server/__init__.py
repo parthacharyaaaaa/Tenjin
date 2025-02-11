d6d09805d221d974d38d948fa8110fa5bf2d4a79
Parth Acharya <1008parth@gmail.com>
2025-02-11 22:28:18 +0530

    Updates __init__ to register app blueprints

--- resource_server/__init__.py
+++ resource_server/__init__.py
@@ -6,10 +6,17 @@ from flask_migrate import Migrate
 
 from resource_server.flask_config import FLASK_CONFIG_OBJECT
 
+from resource_server.blueprint_forum import forum
+from resource_server.blueprint_admin import admin
+from resource_server.blueprint_user import user
+
 app = Flask(import_name="RS",
             instance_path=os.path.join(os.path.dirname(__file__), "instance"))
 
 app.config.from_object(FLASK_CONFIG_OBJECT)
+app.register_blueprint(forum)
+app.register_blueprint(admin)
+app.register_blueprint(user)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
