3aa0c4d5463b3bb2ec148a7efe4323310f52df17
Parth Acharya <1008parth@gmail.com>
2025-02-14 00:04:49 +0530

    Registers endpoint to Flask app

--- resource_server/__init__.py
+++ resource_server/__init__.py
@@ -9,6 +9,7 @@ from resource_server.flask_config import FLASK_CONFIG_OBJECT
 from resource_server.blueprint_forum import forum
 from resource_server.blueprint_admin import admin
 from resource_server.blueprint_user import user
+from resource_server.blueprint_config import config
 
 app = Flask(import_name="RS",
             instance_path=os.path.join(os.path.dirname(__file__), "instance"))
@@ -17,6 +18,7 @@ app.config.from_object(FLASK_CONFIG_OBJECT)
 app.register_blueprint(forum)
 app.register_blueprint(admin)
 app.register_blueprint(user)
+app.register_blueprint(config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
