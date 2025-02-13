e775e9588a7ada0b5ab81ddce2d54257f0bc5186
Parth Acharya <1008parth@gmail.com>
2025-02-13 22:51:30 +0530

    Adds posts table

--- resource_server/models.py
+++ resource_server/models.py
@@ -1,8 +1,8 @@
 from resource_server import db
 
-from sqlalchemy import UniqueConstraint, PrimaryKeyConstraint, Index, CheckConstraint, Extract
+from sqlalchemy import UniqueConstraint, PrimaryKeyConstraint, Index, CheckConstraint, Extract, ForeignKeyConstraint
 from sqlalchemy.dialects.postgresql import TIMESTAMP, BYTEA
-from sqlalchemy.types import INTEGER, NUMERIC, BOOLEAN, VARCHAR, BIGINT
+from sqlalchemy.types import INTEGER, NUMERIC, BOOLEAN, VARCHAR, BIGINT, TEXT
 
 class User(db.Model):
     __tablename__ = "users"
@@ -36,4 +36,35 @@ class User(db.Model):
         CheckConstraint("LENGTH(username) > 5", name="ck_users_username_length"),
         CheckConstraint("_alias IS NULL OR LENGTH(_alias) > 5", name="ck_users_alias_length"),
         CheckConstraint(r"email ~*'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'", name="ck_users_email_regex"),
+    )
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    # Basic identification
+    id = db.Column(BIGINT, nullable = False, autoincrement = True)
+    author_id = db.Column(VARCHAR(64), nullable = False)
+    author_uname = db.Column(VARCHAR(64), nullable = False)
+    forum = db.Column(VARCHAR(128), nullable = False)
+
+    # Post statistics
+    score = db.Column(INTEGER, default = 0)
+    total_comments = db.Column(INTEGER, default = 0)
+
+    # Post details
+    title = db.Column(VARCHAR(64), nullable = False)
+    body_text = db.Column(TEXT, nullable = False)
+    flair = db.Column(VARCHAR(16))
+    closed = db.Column(BOOLEAN, default=False)
+    time_posted = db.Column(TIMESTAMP, nullable=False)
+    saves = db.Column(INTEGER, default=0)
+    reports = db.Column(INTEGER, default=0)
+
+    __table_args__ = (
+        PrimaryKeyConstraint(id, name="pk_posts_id"),
+        ForeignKeyConstraint(author_id, User.id, name="fk_posts_author_id_users_id"),
+        ForeignKeyConstraint(author_uname, User.username, name="fk_posts_author_uname_users_username"),
+        Index("idx_posts_author_id", author_id),
+        Index("idx_posts_author_name", author_uname),
+        Index("idx_posts_title", title)
     )
