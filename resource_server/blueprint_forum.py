5254e8af8948c353b381611f386a165c44057b1e
Parth Acharya <1008parth@gmail.com>
2025-02-14 00:21:48 +0530

    Adds forums outline

--- resource_server/blueprint_forum.py
+++ resource_server/blueprint_forum.py
@@ -1,6 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 forum = Blueprint(__file__.split(".")[0], __file__.split(".")[0], url_prefix="/animes")
 
-@forum.route("/", methods=["GET", "HEAD"])
+@forum.route("/", methods=["GET", "HEAD", "OPTIONS"])
 def index():
-    return "<!DOCTYPE HTML> <html> Hello :3 </html>"
+    ...
+
+@forum.route("/", methods=["POST", "OPTIONS"])
+def create_forum() -> Response:
+    ...
+
+@forum.route("/", methods = ["DELETE", "OPTIONS"])
+def delete_forum() -> Response:
+    ...
+
