5b3b85e7a3660ef471f6de21c463bfe647a4827d
Parth Acharya <1008parth@gmail.com>
2025-02-14 00:09:11 +0530

    Adds outline for user endpoints

--- resource_server/blueprint_user.py
+++ resource_server/blueprint_user.py
@@ -1,2 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 user = Blueprint(__file__.split(".")[0], __file__.split(".")[0], url_prefix="/users")
+
+@user.route("/login", methods=["POST", "OPTIONS"])
+def login() -> Response:
+    ...
+
+@user.route("/", methods=["POST", "OPTIONS"])
+def register() -> Response:
+    ...
+
+@user.route("/", methods=["DELETE"])
+def delete_user() -> Response:
+    ...
+
+@user.route("/", methods=["GET", "HEAD", "OPTIONS"])
+def get_users() -> Response:
+    ...
+
+@user.route("/logout", methods=["DELETE", "OPTIONS"])
+def logout() -> Response:
+    ...
