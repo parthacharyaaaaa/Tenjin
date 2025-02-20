8d4914c03d26a4fef444204cf899b7fe503a1724
Parth Acharya <1008parth@gmail.com>
2025-02-20 22:19:30 +0530

    Adds GET /<int:post_id>

--- resource_server/blueprint_posts.py
+++ resource_server/blueprint_posts.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, jsonify
 post = Blueprint("post", "post", url_prefix="/posts")
+
+from resource_server import db
+
+from sqlalchemy import select
+from resource_server.models import Post
+
+from werkzeug.exceptions import NotFound
 
 @post.route("/", methods=["POST", "OPTIONS"])
 def create_post() -> Response:
@@ -7,7 +14,11 @@ def create_post() -> Response:
 
 @post.route("/<int:post_id>", methods=["GET", "OPTIONS"])
 def get_post(post_id : int) -> Response:
-    ...
+    post : Post | None = db.session.execute(select(Post).where(Post.id == post_id)).scalar_one_or_none()
+    if not post:
+        raise NotFound(f"Post with id {post_id} could not be found :(")
+    return jsonify(post.__json_like__()), 200
+
 
 @post.route("/<int:post_id>", methods=["PATCH", "OPTIONS"])
 def edit_post(post_id : int) -> Response:
