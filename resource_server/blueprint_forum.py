f340656afce88980cf99ee92db8cc030d5c3d40b
Parth Acharya <1008parth@gmail.com>
2025-04-05 14:18:47 +0530

    Adds DELETE /forums/{id}

--- resource_server/blueprint_forum.py
+++ resource_server/blueprint_forum.py
@@ -1,12 +1,15 @@
-from auxillary.decorators import enforce_json
+from auxillary.decorators import enforce_json, token_required
 
 from resource_server.external_extensions import RedisInterface
-from auxillary.utils import rediserialize
+from auxillary.utils import rediserialize, genericDBFetchException
 
-from resource_server.models import db, Forum, User
+from resource_server.models import db, Forum, User, ForumAdmin
 from sqlalchemy import select, update
+from sqlalchemy.exc import SQLAlchemyError
 
-from werkzeug.exceptions import BadRequest, NotFound
+from datetime import datetime
+
+from werkzeug.exceptions import BadRequest, NotFound, Forbidden
 from flask import Blueprint, Response, g, jsonify
 forum = Blueprint(__file__.split(".")[0], __file__.split(".")[0], url_prefix="/forums")
 
@@ -16,9 +19,10 @@ def index():
 
 @forum.route("/", methods=["POST", "OPTIONS"])
 @enforce_json
+@token_required
 def create_forum() -> tuple[Response, int]:
     try:
-        userID: int = int(g.REQUEST_JSON.pop('user_id'))
+        userID: int = g.decodedToken['sid']
         forumName: str = str(g.REQUEST_JSON.pop('forum_name')).strip()
         if not forumName:
             raise ValueError()
@@ -46,7 +50,30 @@ def create_forum() -> tuple[Response, int]:
 
     return jsonify({"message" : "Forum created succesfully"}), 202
 
-@forum.route("/", methods = ["DELETE", "OPTIONS"])
-def delete_forum() -> Response:
-    ...
+@forum.route("/<int:forum_id>", methods = ["DELETE", "OPTIONS"])
+@token_required
+def delete_forum(forum_id: int) -> Response:
+    try:
+        # Check to see if forum exists
+        forum: Forum = db.session.execute(select(Forum).where(Forum.id == forum_id).with_for_update()).scalar_one_or_none()
+        if not forum:
+            raise NotFound(f"Failed to find a forum with this id ({forum_id})")
+        
+        # Check to see if user is owner or superuser
+        userAdmin: ForumAdmin = db.session.execute(select(ForumAdmin).where((ForumAdmin.forum_id == forum_id) & (ForumAdmin.user_id == g.decodedToken['sid']))).scalar_one_or_none()
+        if not userAdmin or userAdmin.role != 'owner' and userAdmin.role != 'super':
+            raise Forbidden("You do not have the necessary permissions to delete this forum")
+        
+    except SQLAlchemyError: raise genericDBFetchException()
+    except KeyError: raise BadRequest("Missing mandatory field 'sid', please login again")
 
+    # Request carries the necessary permissions to delete this forum
+    try:
+        db.session.execute(update(Forum).where(Forum.id == forum_id).values(deleted=True, time_deleted=datetime.now()))
+        db.session.commit()
+    except: 
+        exc: Exception = Exception()
+        exc.__setattr__('description', 'Failed to delete this forum. Please try again later')
+        raise exc
+    
+    return jsonify({'message' : 'forum deleted'}), 200
