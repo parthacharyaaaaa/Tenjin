872f937d99bd68800933ebe963dd68a9be27437d
Parth Acharya <102172409+parthacharyaaaaa@users.noreply.github.com>
2025-04-06 18:56:09 +0530

    Delete 2 directory

--- 2/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
- $host = "localhost";
- $user = "root";                     //Your Cloud 9 username
- $pass = "";                                  //Remember, there is NO password by default!
- $db = "userform";                                  //Your database name you want to connect to
- //$port = 3306;
- $con = mysqli_connect($host, $user, $pass, $db)or die(mysql_error());
-// $con = mysqli_connect('localhost', 'root', '', 'userform');
-?>
