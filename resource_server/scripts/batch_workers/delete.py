5c9d1e9b25335aa70d40af8927c789e74d576e99
Parth Acharya <1008parth@gmail.com>
2025-04-07 12:19:43 +0530

    Reworks background batch deletion logic for strong entities

--- resource_server/scripts/batch_workers/delete.py
+++ resource_server/scripts/batch_workers/delete.py
@@ -1,18 +1,22 @@
-from redis import Redis
-import psycopg2 as pg
-from psycopg2.extras import execute_batch
-from dotenv import load_dotenv
+''' #### Script for perfoming deleion on strong entities'''
 import os
-import time
-from batch_workers.worker_utils import fetchPKColNames
-from traceback import format_exc
+from dotenv import load_dotenv
+
+import psycopg2 as pg
+
 import json
+from time import sleep
+from traceback import format_exc
+
+from worker_utils import fetchDeletions
 
 loaded = load_dotenv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env"))
 if not loaded:
     raise FileNotFoundError()
 
 ID: int = os.getpid()
+
+STRONG_ENTITIES: list[str] = ["users", "forums", "posts", "comments"] #TODO: Add logic to dynamically fetch strong entities (Independent PK)\
 
 CONNECTION_KWARGS : dict[str, int | str] = {
     "user" : os.environ["POSTGRES_USERNAME"],
@@ -29,38 +33,43 @@ except Exception as e:
     exit(500)
 
 if __name__ == "__main__":
-    tempTable: str = f"deletion_table_{ID}_{str(int(time.time()))}"
+    # Load basic config
     with open(os.path.join(os.path.dirname(__file__), "worker_config.json"), 'rb') as configFile:
         configData: dict = json.loads(configFile.read())
-        wait: float = configData.get("wait", 1)
-        backoffSequence: list[float] = configData.get("backoff_seq", [0.1, 0.5, 1, 2, 3])
+        wait: float = configData.get("wait", 5)
+        batchSize: int = configData["delete_batch_size"]
     
-
-    DELETION_SQL: str = "DELETE FROM {table_name} USING {temp_del_table} WHERE {clause}"
-    ERROR_SQL: str = "INSERT INTO delete_errors VALUES %s ON CONFLICT DO NOTHING;"
-    query_groups: dict[str, list[tuple[int]]] = {}       # dict[<tablename> : list[tuple[PK values in <tablename>]]]
+    
+    DELETION_SQL: str = "DELETE FROM {table_name} WHERE id IN ({ids_to_delete});"
     dbCursor: pg.extensions.cursor = CONNECTION.cursor()
 
-    # Some logic to xread 1k from insert consumer group
-    # some logic to sort queries into respective buckets
     with dbCursor as dbCursor:
-        for table, pkargs in query_groups.items():
-            # Create temp table 
-            # For deletion, we'll need to dynamically create the comparison logic by fetching the PK column names of each table
-            pk_cols: list[str] = fetchPKColNames(dbCursor, table)
+        while(True):
+            for table in STRONG_ENTITIES:
+                hitlist = fetchDeletions(dbCursor, table)
+                if not hitlist:
+                    continue
 
-            DELETION_WHERE_CLAUSE: str = " AND ".join(f"{table}.{col} = {tempTable}.{col}" for col in pk_cols)
+                dbCursor.execute(f"SAVEPOINT s{ID}")
+                try:
+                    dbCursor.execute(query=DELETION_SQL.format(table_name = table, ids_to_delete = ', '.join(hitlist)))
 
-            dbCursor.execute(f"CREATE TABLE {tempTable} AS SELECT {', '.join([col for col in pk_cols])} FROM {table} WHERE 1 = 0 LIMIT 1;")
+                    CONNECTION.commit()
 
-            execute_batch(cur=dbCursor,
-                          sql=f"INSERT INTO {tempTable} VALUES %s",
-                          argslist=pkargs)
-            CONNECTION.commit()
+                except (pg.errors.ModifyingSqlDataNotPermitted):
+                    print(f"[{ID}]: Permission error, aborting script...")
+                    exit(500)
+                except (pg.errors.SyntaxError, pg.errors.AmbiguousColumn, pg.errors.AmbiguousParameter) as e:
+                    print(f"[{ID}]: SQL invalid, aborting script, please manually resolve insertion logic...")
+                    print(f"[{ID}]: Traceback: {e.__class__.__name__}\n{format_exc()}")
+                    exit(500)
+                except (pg.errors.FdwTableNotFound, pg.errors.UndefinedTable):
+                    print(f"[{ID}]: Table not found")
+                    dbCursor.execute(f"ROLLBACK TO s{ID}")
+                except pg.errors.Error as pg_error:
+                    print(f"[{ID}]: Error in executing batch delete for table {table}, exception: {pg_error.__class__.__name__}")
+                    print(f"[{ID}]: Error details: {format_exc()}")
+                    dbCursor.execute(f"ROLLBACK TO SAVEPOINT s{ID}")
 
-            dbCursor.execute(DELETION_SQL.format(table_name = table, 
-                                                 temp_del_table = tempTable,
-                                                 clause=DELETION_WHERE_CLAUSE))
-            dbCursor.execute(f"DROP TABLE {tempTable}")
-
-            CONNECTION.commit()
+            # Good night >:3
+            sleep(wait)
