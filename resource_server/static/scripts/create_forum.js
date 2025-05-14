0a887a77c7de2a42726a7a16a6aa2ff7ae7faa3f
Parth Acharya <1008parth@gmail.com>
2025-05-14 14:31:31 +0530

    Reworks logic for creating a new forum in an anime

---
+++ resource_server/static/scripts/create_forum.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const animeID = window.location.pathname.split("/")[3];
+    const forumPromptWindow = document.getElementById('forum-modal');
+    const forumPromptWindowBox = document.getElementById("forum-modal-box");
+    const createForumButton = document.getElementById('forum-make-btn');
+
+    window.dependencyReady?.then(async () => {
+        if (!localStorage.getItem('login')) {
+            createForumButton.innerText = 'Login to create a forum';
+            createForumButton.addEventListener('click', () => {
+                window.location.href = '/login';
+            });
+            return;
+        }
+
+        createForumButton.addEventListener('click', () => {
+            forumPromptWindow.classList.remove('hidden');
+            setTimeout(() => {
+                const outsideClickHandler = (event) => {
+                    if (!forumPromptWindowBox.contains(event.target)) {
+                        forumPromptWindow.classList.add('hidden');
+                        document.removeEventListener('click', outsideClickHandler);
+                    }
+                };
+
+                document.addEventListener('click', outsideClickHandler);
+            }, 0.1);
+
+            const submitButton = document.getElementById('forum-submit');
+            submitButton.addEventListener('click', async () => {
+                try {
+                    fname = document.getElementById('forum-name').value;
+                    if (!fname || fname === undefined || fname.trim().length < 6) {
+                        alert("Invalid forum name");
+                        return;
+                    }
+
+                    fdesc = document.getElementById('forum-desc').value;
+
+                    const response = await fetch('/forums', {
+                        method: "POST",
+                        credentials: 'include',
+                        body: JSON.stringify({ forum_name: fname, desc: fdesc ? null : fdesc, anime_id: animeID }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    });
+
+                    if (!response.ok) {
+                        throw new Error();
+                    }
+
+                    alert("Forum made!");
+                    forumPromptWindow.classList.add('hidden');
+                    document.removeEventListener('click', outsideClickHandler);
+
+                }
+                catch (error) {
+                    console.error("Failed to create a forum");
+                    forumPromptWindow.classList.remove('hidden');
+                }
+            })
+        });
+    });
+});
