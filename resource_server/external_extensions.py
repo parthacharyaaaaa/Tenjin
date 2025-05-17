7c1a4697cf442a5da4e72da212fa93c21405a0b3
Parth Acharya <1008parth@gmail.com>
2025-05-17 16:25:32 +0530

    Adds batch hset function

--- resource_server/external_extensions.py
+++ resource_server/external_extensions.py
@@ -1,5 +1,6 @@
 from redis import Redis
 from flask import Flask
+from typing import Iterable
 
 RedisInterface = None
 
@@ -7,6 +8,16 @@ def hset_with_ttl(interface: Redis, name: str, mapping: dict, ttl: int, transact
     with interface.pipeline(transaction) as pp:
         pp.hset(name=name, mapping=mapping)
         pp.expire(name=name, time=ttl)
+        pp.execute()
+
+def batch_hset_with_ttl(interface: Redis, names: Iterable[str], mappings: Iterable[dict], ttl: int, transaction: bool = True):
+    if len(names) != len(mappings):
+        raise ValueError('Names and mappings do not match')
+    
+    with interface.pipeline(transaction) as pp:
+        for idx, mapping in enumerate(mappings):
+            pp.hset(name=names[idx], mapping=mapping)
+            pp.expire(name=names[idx], time=ttl)
         pp.execute()
 
 def init_redis(app: Flask):
