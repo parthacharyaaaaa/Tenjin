e61930848e2ac499ebf2ba5d71459b19bf34f9d6
Parth Acharya <1008parth@gmail.com>
2025-05-18 16:27:32 +0530

    Adds update_jwks method

--- auth_server/keygen.py
+++ auth_server/keygen.py
@@ -2,6 +2,12 @@ import ecdsa
 from hashlib import sha512
 import os
 from cryptography.fernet import Fernet
+import base64
+import ujson
+
+def int_to_base64url(n: int) -> str:
+    byte_length = (n.bit_length() + 7) // 8
+    return base64.urlsafe_b64encode(n.to_bytes(byte_length, 'big')).rstrip(b'=').decode('ascii')
 
 def generate_ecdsa_pair() -> tuple[ecdsa.SigningKey, ecdsa.VerifyingKey]:
     '''Generate signing and verification ECDSA key pair'''
@@ -9,6 +15,29 @@ def generate_ecdsa_pair() -> tuple[ecdsa.SigningKey, ecdsa.VerifyingKey]:
     verificiationKey: ecdsa.VerifyingKey = signingKey.get_verifying_key()
 
     return signingKey, verificiationKey
+
+def update_jwks(vk: ecdsa.VerifyingKey, kid: int,
+                jwks_json_filepath: os.PathLike,
+                enforce_capacity: bool = True,
+                capacity: int = 3) -> None:
+    '''Updates the JWKS JSON file to include the given public key as the latest key'''
+    keyMapping: dict[str, str|int] = {'kty' : 'EC', 'alg' : 'ECDSA', 'crv' : ecdsa.SECP256k1.__str__(), 'use' :'sig', 'kid' : kid}
+    encodedX, encodedY = int_to_base64url(vk.pubkey.point.x()), int_to_base64url(vk.pubkey.point.y())
+
+    keyMapping.update({'x' : encodedX, 'y' : encodedY})
+
+    with open(jwks_json_filepath, 'r+') as jwks_json_file:
+        jwks_contents: list[dict[str, str|int]] = ujson.loads(jwks_json_file.read())['keys']
+        jwks_contents.append(keyMapping)
+        length: int = len(jwks_contents)
+
+        if enforce_capacity and length > capacity:
+            jwks_contents: list[dict[str, str|int]] = jwks_contents[-capacity:]
+        
+        jwks_json_file.seek(0)
+        jwks_json_file.write(ujson.dumps(jwks_contents))
+        ujson.dump({'keys' : jwks_contents}, jwks_json_filepath)
+        jwks_json_file.truncate()
 
 def write_ecdsa_pair(privateDir: os.PathLike, staticDir: os.PathLike,
                      encryption_key: bytes,
