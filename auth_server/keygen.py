32ed5acc9de8b0c212347c91ad6571fbebc72b86
Parth Acharya <1008parth@gmail.com>
2025-05-18 15:48:35 +0530

    Updates ecdsa logic

--- auth_server/keygen.py
+++ auth_server/keygen.py
@@ -3,16 +3,16 @@ from hashlib import sha512
 import os
 from cryptography.fernet import Fernet
 
-def generate_ecdsa_pair() -> tuple[bytes, bytes]:
+def generate_ecdsa_pair() -> tuple[ecdsa.SigningKey, ecdsa.VerifyingKey]:
     '''Generate signing and verification ECDSA key pair'''
     signingKey: ecdsa.SigningKey = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=sha512)
     verificiationKey: ecdsa.VerifyingKey = signingKey.get_verifying_key()
 
-    return signingKey.to_pem(), verificiationKey.to_pem()
+    return signingKey, verificiationKey
 
 def write_ecdsa_pair(privateDir: os.PathLike, staticDir: os.PathLike,
                      encryption_key: bytes,
-                     private_key: bytes, public_key: bytes, key_id: int,
+                     private_key: ecdsa.SigningKey, public_key: ecdsa.VerifyingKey, key_id: int,
                      fname_template: str = '{key_type}_{key_id}_key.pem') -> None:
     ''' ### Write the private and public keys in their respective PEM files
     
@@ -27,14 +27,14 @@ def write_ecdsa_pair(privateDir: os.PathLike, staticDir: os.PathLike,
     '''
     fernet = Fernet(encryption_key)
 
-    encryptedPrivateKey: bytes = fernet.encrypt(private_key)
+    encryptedPrivateKey: bytes = fernet.encrypt(private_key.to_pem())
     privateFpath: os.PathLike = os.path.join(privateDir, fname_template.format(key_type='private', key_id=key_id))
-    publicFpath: os.PathLike = os.path.join(privateDir, fname_template.format(key_type='public', key_id=key_id))
+    publicFpath: os.PathLike = os.path.join(staticDir, fname_template.format(key_type='public', key_id=key_id))
 
     with open(privateFpath, 'wb+') as privatePemFile:
         privatePemFile.write(encryptedPrivateKey)
 
     with open(publicFpath, 'wb+') as publicPemFile:
-        publicPemFile.write(public_key)
+        publicPemFile.write(public_key.to_pem())
 
     os.chmod(privateFpath, 0o600)
