b6f3cb3a8267da0b9853de7fae70b2f32205fc17
Parth Acharya <1008parth@gmail.com>
2025-05-19 19:28:13 +0530

    Updates decorators token_required & pass_user_details

--- auxillary/decorators.py
+++ auxillary/decorators.py
@@ -1,9 +1,13 @@
 from functools import wraps
+import ecdsa.ellipticcurve
 from flask import request, current_app, g
 from werkzeug.exceptions import BadRequest, Unauthorized, InternalServerError
 from datetime import timedelta
-from jwt import decode
+from jwt import decode, get_unverified_header
 from jwt.exceptions import PyJWTError, ExpiredSignatureError
+import requests
+import base64
+import ecdsa
 
 def enforce_json(endpoint):
     @wraps(endpoint)
@@ -33,24 +37,59 @@ def require_intraservice_key(endpoint):
 def token_required(endpoint):
     '''
     Protect an endpoint by validating an access token. Requires the header "Authorization: Bearer <credentials>". 
-    Furthermore, sets global data (flask.g : _AppCtxGlobals) for usage of token details in the decorated endpoint
+    Furthermore, sets global data (flask.g.decodedToken : _AppCtxGlobals) for usage of token details in the decorated endpoint
+
+    Uses flask.current_app.config['key_mapping'] to check for valid verification keys. On failure, pings the JWKS endpoint to check for the key's existence. If found, updates the mapping with the new key and then performs verification
     '''
     @wraps(endpoint)
     def decorated(*args, **kwargs):
-        try:
-            auth_metadata = request.cookies.get("access", request.cookies.get("Access"))
-            if not auth_metadata:
-                raise Unauthorized("Authentication details missing")
-            decodedToken = decode(
-                                jwt=auth_metadata.split()[-1],
-                                key=current_app.config["SIGNING_KEY"],
-                                algorithms=["HS256"],
-                                issuer="babel-auth-service",
-                                leeway=timedelta(minutes=3)
-            )
+        encodedAccessToken = request.cookies.get("access", request.cookies.get("Access"))
+        if not encodedAccessToken:
+            raise Unauthorized("Authentication details missing")
+        
+        headers: dict[str, str|int] = get_unverified_header(encodedAccessToken)
+        tokenKID, alg = headers.get('KID'), headers.get('alg')
+        if not tokenKID:
+            raise Unauthorized("Invalid token, key ID missing")
+        
+        if alg != 'ES256':
+            raise Unauthorized("Invalid token, unsupported algorithm claim")
+        
+        try: 
+            decodedToken: dict = None
+            if tokenKID in current_app.config['KNOWN_KEYS']:
+                decodedToken = decode(jwt=encodedAccessToken,
+                                      key=current_app.config['KNOWN_KEYS'][tokenKID],
+                                      algorithms=["ES256"],
+                                      leeway=timedelta(minutes=3))
+                
+            # Possibly new KID, ping auth server
+            else:
+                response = requests.get(f'{current_app.config["AUTH_SERVER_URL"]}/jwks.json', timeout=3)
+                if response.status_code != 200:
+                    raise Unauthorized("Failed to validate JWT. This may be an issue with our authentication service")
+                
+                newMapping: dict[str, str|int] = response.json()['keys']
+                # For any new items in newMapping, we'll need to construct a new dict entry with its public verificiation key
+                for keyMetadata in newMapping:
+                    # New key found, welcome to the club >:3
+                    if keyMetadata['kid'] not in current_app.config['KNOWN_KEYS']:
+                        x = int.from_bytes(base64.urlsafe_b64decode(keyMetadata['x']).decode(), 'big', True)
+                        y = int.from_bytes(base64.urlsafe_b64decode(keyMetadata['y']), 'big', True)
+                        point = ecdsa.ellipticcurve.Point(ecdsa.SECP256k1.curve, x, y)
+                        vk = ecdsa.VerifyingKey.from_public_point(point, curve=ecdsa.SECP256k1)
+
+                        current_app.config['KNOWN_KEYS'][keyMetadata['kid']] = vk.to_pem()
+
+                if tokenKID not in current_app.config['KNOWN_KEYS']:
+                    raise Unauthorized('Invalid Key ID, no such key was found. Please login again')
+                
+                decodedToken = decode(jwt=encodedAccessToken,
+                                      key=current_app.config['KNOWN_KEYS'][tokenKID],
+                                      algorithms=['ES256'],
+                                      leeway=timedelta(minutes=3))
+
             g.decodedToken = decodedToken
-        except KeyError as e:
-            raise BadRequest(f"Endpoint /{request.path[1:]} requires an authorization token to give access to resource")
         except ExpiredSignatureError:
             raise Unauthorized("JWT token expired, begin refresh issuance")
         except PyJWTError as e:
@@ -62,25 +101,60 @@ def token_required(endpoint):
 def pass_user_details(endpoint):
     '''
     Pass user details by parssing an access token. Requires the header "Authorization: Bearer <credentials>". 
-    Furthermore, sets global data (flask.g : _AppCtxGlobals) for usage of token details in the decorated endpoint
+    Furthermore, sets global data (flask.g.REQUESTING_USER : _AppCtxGlobals) for usage of token details in the decorated endpoint
     In case of any errors, fall backs to None
     '''
+    #NOTE: Perhaps use Flask.g to include any authentication errors too?
     @wraps(endpoint)
     def decorated(*args, **kwargs):
-        auth_metadata = request.cookies.get("access", request.cookies.get("Access"))
-        if not auth_metadata:
-            g.requestUser = None
-        else:
-            try:
-                decodedToken = decode(
-                                    jwt=auth_metadata.split()[-1],
-                                    key=current_app.config["SIGNING_KEY"],
-                                    algorithms=["HS256"],
-                                    issuer="babel-auth-service",
-                                    leeway=timedelta(minutes=3))
-                g.requestUser = decodedToken
-            except Exception:
-                g.requestUser = None
+        g.REQUESTING_USER = None
+        encodedAccessToken = request.cookies.get("access", request.cookies.get("Access"))
+        if not encodedAccessToken:
+            return endpoint(*args, **kwargs)
+        
+        headers: dict[str, str|int] = get_unverified_header(encodedAccessToken)
+        tokenKID, alg = headers.get('KID'), headers.get('alg')
+        if not tokenKID:
+            return endpoint(*args, **kwargs)
+        
+        if alg != 'ES256':
+            return endpoint(*args, **kwargs)
+        
+        try: 
+            if tokenKID in current_app.config['KNOWN_KEYS']:
+                decode(jwt=encodedAccessToken,
+                                      key=current_app.config['KNOWN_KEYS'][tokenKID],
+                                      algorithms=["ES256"],
+                                      leeway=timedelta(minutes=3))
+                
+            # Possibly new KID, ping auth server
+            else:
+                response = requests.get(f'{current_app.config["AUTH_SERVER_URL"]}/jwks.json', timeout=3)
+                if response.status_code != 200:
+                    return endpoint(*args, **kwargs)
+                
+                newMapping: dict[str, str|int] = response.json()['keys']
+                # For any new items in newMapping, we'll need to construct a new dict entry with its public verificiation key
+                for keyMetadata in newMapping:
+                    # New key found, welcome to the club >:3
+                    if keyMetadata['kid'] not in current_app.config['KNOWN_KEYS']:
+                        x = int.from_bytes(base64.urlsafe_b64decode(keyMetadata['x']).decode(), 'big', True)
+                        y = int.from_bytes(base64.urlsafe_b64decode(keyMetadata['y']), 'big', True)
+                        point = ecdsa.ellipticcurve.Point(ecdsa.SECP256k1.curve, x, y)
+                        vk = ecdsa.VerifyingKey.from_public_point(point, curve=ecdsa.SECP256k1)
+
+                        current_app.config['KNOWN_KEYS'][keyMetadata['kid']] = vk.to_pem()
+
+                if tokenKID not in current_app.config['KNOWN_KEYS']:
+                    return endpoint(*args, **kwargs)
+                
+                decode(jwt=encodedAccessToken,
+                                      key=current_app.config['KNOWN_KEYS'][tokenKID],
+                                      algorithms=['ES256'],
+                                      leeway=timedelta(minutes=3))
+
+        except Exception:
+            return endpoint(*args, **kwargs)
         
         return endpoint(*args, **kwargs)
     return decorated
