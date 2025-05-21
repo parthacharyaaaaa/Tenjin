54f92df392623bc120b55572d35bf3cd28beb29e
Parth Acharya <1008parth@gmail.com>
2025-05-21 12:38:32 +0530

    Adds SyncedStore

--- auth_server/redis_manager.py
+++ auth_server/redis_manager.py
@@ -1,6 +1,7 @@
 from redis import Redis
 from flask import Flask
 
+SyncedStore: Redis = None
 RedisInterface: Redis = None
 
 def init_redis(app: Flask) -> None:
@@ -9,4 +10,12 @@ def init_redis(app: Flask) -> None:
     RedisInterface = Redis(**app.config['REDIS_KWARGS'])
 
     if not RedisInterface.ping():
+        raise ConnectionError('Failed to connect to Redis instance')
+
+def init_syncedstore(app: Flask) -> None:
+    global SyncedStore
+    '''Initialize a Redis instance based on a Flask app's "REDIS_SYNCED_STORE_KWARGS" config key'''
+    SyncedStore = Redis(**app.config['REDIS_SYNCED_STORE_KWARGS'])
+
+    if not SyncedStore.ping():
         raise ConnectionError('Failed to connect to Redis instance')
