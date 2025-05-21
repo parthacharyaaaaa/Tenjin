5d5859c9e097b9a764daed65e642450a30956bcc
Parth Acharya <1008parth@gmail.com>
2025-05-21 12:39:15 +0530

    Reworks entire app factory

--- auth_server/auth_app.py
+++ auth_server/auth_app.py
@@ -2,111 +2,162 @@ from flask import Flask
 from auth_server.config import flaskconfig
 import time
 import os, ujson
-from jsonschema import validate
+from sqlalchemy import text
 import ecdsa
-from auxillary.utils import generic_error_handler
+from datetime import datetime
+from auxillary.utils import generic_error_handler, to_base64url
 from auth_server.keygen import generate_ecdsa_pair, write_ecdsa_pair
 from auth_server.key_container import KeyMetadata
 from cryptography.fernet import Fernet
+import traceback
 
 APP_CTX_CWD: os.PathLike = os.path.dirname(__file__)
 def create_app() -> Flask:
     auth_app = Flask('auth_app', instance_path=os.path.join(APP_CTX_CWD, 'instance'), static_folder=os.path.join(APP_CTX_CWD, 'static'))
     auth_app.config.from_object(flaskconfig)
+    auth_app.pid = os.getpid()
 
     # Error handler
     auth_app.register_error_handler(Exception, generic_error_handler)
 
-    # Extensions
-    from auth_server.redis_manager import init_redis
-    init_redis(auth_app)
+    # SQLAlchemy
+    from auth_server.models import db
+    from flask_migrate import Migrate
+    db.init_app(auth_app)
+    migrate = Migrate(auth_app, db)
 
-    from auth_server.redis_manager import RedisInterface
+    # Extensions
+    from auth_server.redis_manager import init_redis, init_syncedstore
+    init_redis(auth_app)
+    init_syncedstore(auth_app)
+    from auth_server.redis_manager import RedisInterface, SyncedStore
+
     from auth_server.token_manager import init_token_manager
 
-    # Load JWKS on startup, if none found, assume fresh start and initiate a new store
-    #TODO: Set master system because Gunicorn will start up multiple workers, and we only need a single key pair on fresh starts (Redis flag with 'master:os.pid()' should do it)
-    initialMap: dict = None
+    isMaster: bool = bool(SyncedStore.set('AUTH_BOOTUP_MASTER', auth_app.pid, nx=True))
 
-    # Set filepaths
-    JWKS_FPATH: os.PathLike = os.path.join(APP_CTX_CWD, 'instance', auth_app.config['JWKS_FILENAME'])
-    private_fpath: os.PathLike = os.path.join(APP_CTX_CWD, 'instance', 'keys')
-    public_fpath: os.PathLike = os.path.join(APP_CTX_CWD, auth_app.static_folder, 'keys')
+    if not isMaster:
+        # Wait for master worker to finish managing key synchronization and file I/O, and then proceed on the assumption that the JWKS file has been written into/validated.
+        while SyncedStore.get('AUTH_BOOTUP_MASTER'):
+            time.sleep(1)
 
-    # Lets get started >:3
-    with open(JWKS_FPATH, 'r') as jwks_file:
-        content: str = jwks_file.read()
-        if content:
-            initialMap: dict = ujson.loads(content)
-    
-    with open(os.path.join(auth_app.static_folder, 'jwks_schema.json')) as schemaFile:
-        schema: dict = ujson.loads(schemaFile.read())
-
-    validSchema: bool = True
-    try:
-        validate(initialMap, schema=schema)
-    except:
-        validSchema = False
-
-    # Exception for empty files
-    if not initialMap:
-        kid, sk, vk = generate_ecdsa_pair()
-
-        # Persist to PEM and JWKS
-        write_ecdsa_pair(privateDir=private_fpath, staticDir=public_fpath,
-                         encryption_key=auth_app.config['PRIVATE_PEM_ENCRYPTION_KEY'],
-                         private_key=sk, public_key=vk, key_id=kid)
-
-        with open(JWKS_FPATH, 'w') as jwks_file:
-            jwks_file.write(ujson.dumps(obj={'keys' :[{'kty' : 'EC',
-                                                'alg' : 'ECDSA',
-                                                'crv' : str(ecdsa.SECP256k1),
-                                                'use' :'sig',
-                                                'kid' : kid,
-                                                'x' : int(vk.pubkey.point.x()), 'y' : int(vk.pubkey.point.y())}]}, indent=2))
+        if SyncedStore.get('ABORT'):
+            print(f'[AUTH {auth_app.pid}] Master failed to setup key configuration, aborting...')
+            raise AssertionError('Master failed to set up key configuration')
         
-        # Finally initialize token manager with the newly generated key metadata
-        init_token_manager(kvsMapping={kid:KeyMetadata(PUBLIC_PEM=vk.to_pem(), PRIVATE_PEM=sk.to_pem(), ALGORITHM='ES256', EPOCH=time.time())},
-                           redisinterface=RedisInterface)
-
-    elif not validSchema:  
-        # Exit early on invalid, non-empty JWKS file
-        print("\n[AUTH] Malformmatted JWKS file found, exiting...")
-        raise ValueError('Malformmatted JWKS file')
+        # Once lock is released, slave worker only needs to consult database and write to its own memory
+        kvs_mapping: dict[str, KeyMetadata] = {}
+        with auth_app.app_context():
+            with db.engine.connect() as conn:
+                res: tuple[tuple] = conn.execute(text('''SELECT kid, public_pem, private_pem, alg, epoch, rotated_out_at
+                                                      FROM keydata
+                                                      WHERE expired_at IS NULL
+                                                      ORDER BY epoch DESC LIMIT 3;''')).fetchall()
+                for keyData in res:
+                    kvs_mapping[keyData[0]] = KeyMetadata(*keyData[1:])
         
-    else:
-        # Valid JWKS file, safe to load into memory and into token manager
-        kvs_mapping: dict = {}
-        fernet: Fernet = Fernet(auth_app.config['PRIVATE_PEM_ENCRYPTION_KEY'])  # Private PEM files are encrypted symmetrically
-        
-        # Load key data mapping for token manager
-        for idx, keyData in enumerate(initialMap['keys']):
-            #TODO: Add slicing for latest n keys only. (Poor intern implemented the bloody thing with win32, bless his heart I couldn't tell him that we deploy on Alpine lulz)
-            privatePem: bytes = None
-            publicPem: bytes = None
-
-            try:
-                # Private Pem
-                with open(os.path.join(private_fpath, f'private_{keyData["kid"]}_key.pem'), 'rb') as privatePemFile:
-                    contents: bytes = privatePemFile.read()
-                    privatePem = fernet.decrypt(contents)
-
-                with open(os.path.join(public_fpath, f'public_{keyData["kid"]}_key.pem'), 'rb') as publicPemFile:
-                    publicPem = publicPemFile.read()
-            except FileNotFoundError:
-                print(f'[AUTH] Missing PEM files for keys present in JWKS, exiting...')
-                raise FileNotFoundError()
-
-            kvs_mapping[keyData['kid']] = KeyMetadata(publicPem, privatePem, 'ES256', keyData.get('epoch', time.time()))
-
+        # Initialize token manager 
         init_token_manager(kvs_mapping, RedisInterface)
-    
-    # This flow also stops the function from creating a million PEM files when debugging, super kewl >:3
 
+    
+    # Current worker process is the master, and is responsible for handling JWKS and key synchronization on bootup
+    else:
+        try:
+            print(f'[AUTH {auth_app.pid}] Serving as master')
+            initialMap: dict = None
+
+            # Set filepaths
+            JWKS_FPATH: os.PathLike = os.path.join(APP_CTX_CWD, 'instance', auth_app.config['JWKS_FILENAME'])
+            private_fpath: os.PathLike = os.path.join(APP_CTX_CWD, 'instance', 'keys')
+            public_fpath: os.PathLike = os.path.join(APP_CTX_CWD, auth_app.static_folder, 'keys')
+
+            # Consult DB
+            kvs_mapping: dict[str, KeyMetadata] = {}
+            writeBuffer: list[dict[str, str|int]] = []
+            with auth_app.app_context():
+                with db.engine.connect() as conn:
+                    res = conn.execute(text('''SELECT kid, public_pem, private_pem, curve, alg, epoch, rotated_out_at
+                                 FROM keydata
+                                 WHERE expired_at IS NULL
+                                 ORDER BY EPOCH DESC;''')).fetchall()
+
+                    if not res:
+                        # No valid keys in DB, master must create new pair
+                        print(f'[AUTH {auth_app.pid}] Creating new key pair')
+                        kid, sk, vk = generate_ecdsa_pair()
+
+                        # Persist to PEM, and DB (JWKS done at end)
+                        write_ecdsa_pair(privateDir=private_fpath, staticDir=public_fpath,
+                                        encryption_key=auth_app.config['PRIVATE_PEM_ENCRYPTION_KEY'],
+                                        private_key=sk, public_key=vk, key_id=kid)
+                            
+                        conn.execute(text('''INSERT INTO keydata (kid, alg, curve, epoch, private_pem, public_pem)
+                                        VALUES (:kid, :alg, :curve, :epoch, :private_pem, :public_pem)'''),
+                                        {'kid':kid, 'alg':'ES256', 'curve':str(ecdsa.SECP256k1), 'epoch':datetime.now(), 'private_pem':sk.to_pem(), 'public_pem':vk.to_pem()})
+                        conn.commit()
+                        kvs_mapping[kid] = KeyMetadata(PUBLIC_PEM=vk.to_pem(), PRIVATE_PEM=sk.to_pem(), ALGORITHM='ES256', EPOCH=time.time())
+                        point = vk.pubkey.point
+                        encodedX, encodedY = to_base64url(int(point.x())), to_base64url(int(point.y()))
+                        writeBuffer.append({"kty": "EC", "alg": 'ES256',"crv": str(ecdsa.SECP256k1), "use": "sig", "kid": kid,
+                                            "x" : encodedX, 
+                                            "y": encodedY})
+
+                    else:
+                        # Atleast 1 non-expired key exists in DB
+                        print(f'[AUTH {auth_app.pid}] Active key(s) found, loading into memory...')
+                        if len(res) > 3:
+                            # Invalidate older keys
+                            conn.execute(text(f'UPDATE keydata SET expired_at = {datetime.now()} WHERE epoch < {res[2][-2]}'))
+                            conn.commit()
+                            res = res[:3]
+                
+                        fernet: Fernet = Fernet(auth_app.config['PRIVATE_PEM_ENCRYPTION_KEY'])
+                        for keyData in res:
+                            # Cast to bytes because memoryview is incompatible with ecdsa.VerifiyingKey.from_pem()
+                            public_pem:bytes = bytes(keyData[1])
+                            private_pem:bytes = bytes(keyData[2])
+                            kvs_mapping[keyData[0]] = KeyMetadata(public_pem, private_pem, keyData[4], keyData[5], keyData[6])
+
+                            # Check if PEM files exist, if not then write new ones
+                            privatePemFile: os.PathLike = os.path.join(private_fpath, f'private_{keyData[0]}_key.pem')
+                            publicPemFile: os.PathLike = os.path.join(public_fpath, f'public_{keyData[0]}_key.pem')
+                            if not os.path.isfile(privatePemFile):
+                                print(f'[AUTH {auth_app.pid}] PEM file {privatePemFile} active in DB but missing, recreating...')
+                                with open(privatePemFile, 'wb+') as newPEM:
+                                    newPEM.write(fernet.encrypt(private_pem))
+
+                            if not os.path.isfile(publicPemFile):
+                                print(f'[AUTH {auth_app.pid}] PEM file {publicPemFile} active in DB but missing, recreating...')
+                                with open(publicPemFile, 'wb+') as newPEM:
+                                    newPEM.write(public_pem)
+
+                            # Append data to writeBuffer
+                            point = ecdsa.VerifyingKey.from_pem(public_pem).pubkey.point
+                            writeBuffer.append({"kty": "EC", "alg": keyData[4],"crv": keyData[3], "use": "sig", "kid": keyData[0],
+                                                "x": to_base64url(int(point.x())), 
+                                                "y": to_base64url(int(point.y()))})
+
+                    # Rewrite JWKS with writeBuffer
+                    print(f'[AUTH {auth_app.pid}] Rewriting JWKS with active keys...')
+                    with open(JWKS_FPATH, 'w') as jwks_file:
+                        jwks_file.write(ujson.dumps({'keys' : writeBuffer}, indent=2))
+
+                    # Initialize token manager
+                    init_token_manager(kvs_mapping, RedisInterface)
+                    print(f'[AUTH {auth_app.pid}] Master process bootup complete!')
+        except Exception as e:
+            print(f'[AUTH {auth_app.pid}] Master worker has encountered an irrecovarable error, details: ')
+            print(traceback.format_exc())
+            SyncedStore.set('ABORT', 1, ex=120)
+            raise RuntimeError('Master bootup failed') from e
+        finally:
+            # Finally, remove the flag from Redis to allow slave workers to continue bootup
+            SyncedStore.delete('AUTH_BOOTUP_MASTER')
+        
     # Blueprints
     from auth_server.blueprint_routes import auth
-    from auth_server.blueprint_cmd import cmd
-    auth_app.register_blueprint(cmd)
+    # from auth_server.blueprint_cmd import cmd
+    # auth_app.register_blueprint(cmd)
     auth_app.register_blueprint(auth)
 
 
