2f9aca7e969c3e465e7b907b31c4a989014687a4
Parth Acharya <1008parth@gmail.com>
2025-05-22 23:22:08 +0530

    Update auth run file

--- auth_run.py
+++ auth_run.py
@@ -1,5 +1,8 @@
 from auth_server.auth_app import create_app
+from auth_server.models import *
 
 if __name__ == '__main__':
     auth_app = create_app()
+    with auth_app.app_context():
+        db.create_all()
     auth_app.run(host=auth_app.config['HOST'], port=auth_app.config['PORT'], debug=True)
