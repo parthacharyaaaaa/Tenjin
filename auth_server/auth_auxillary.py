be71923834d1d88fa3e0c6b69f4e018e7e956303
Parth Acharya <1008parth@gmail.com>
2025-05-26 11:25:41 +0530

    Adds auth server specific helper functions

--- auth_server/auth_auxillary.py
+++ auth_server/auth_auxillary.py
@@ -1,22 +1,22 @@
-from auth_server.models import db, SuspiciousActivity
-from redis_manager import SyncedStore
-from sqlalchemy import func, select, insert
+from auth_server.models import db, SuspiciousActivity, Admin
+from auth_server.redis_manager import SyncedStore
+from sqlalchemy import func, select, insert, update, cast
+from sqlalchemy.dialects.postgresql import TIMESTAMP
 from datetime import datetime
 from flask import current_app
 
-def purge_admin_session(adminID: int, ttl: int) -> None:
-    with SyncedStore.pipeline() as pipe:
-        pipe.delete(f'admin:{adminID}')
-        pipe.set(f'blacklist:{adminID}', 1, ex=ttl, nx=True)
-        pipe.execute()
-
-
-def report_suspicious_activity(adminID: int, desc: str) -> None:
+def report_suspicious_activity(adminID: int, desc: str, force_logout: bool = True) -> None:
     db.session.execute(insert(SuspiciousActivity)
                        .values(suspect=adminID, description='Incorrect password'))
-    db.session.commit()
     current_time: datetime = datetime.now()
 
-    # If too many suspicious attempts, force logout and lock account
-    if db.session.execute(func.count(select(SuspiciousActivity).where((SuspiciousActivity.suspect == adminID) & (SuspiciousActivity.time_logged.between(current_time, current_app.config['SUSPICIOUS_LOOKBACK_TIME']))))) >= current_app.config['MAX_ACTIVITY_LIMIT']:
-        purge_admin_session(SyncedStore, adminID, current_app.config['PURGE_BROADCAST_TTL'])
+    stmt = select(func.count()).select_from(SuspiciousActivity).where(
+        (SuspiciousActivity.suspect == adminID) &
+        (SuspiciousActivity.time_logged.between(current_time-current_app.config['SUSPICIOUS_LOOKBACK_TIME'], current_time))
+    )
+
+    if force_logout and db.session.execute(stmt).scalar() >= current_app.config['MAX_ACTIVITY_LIMIT']:
+        db.session.execute(update(Admin).values(locked=True))
+        SyncedStore.delete(f'admin:{adminID}')
+
+    db.session.commit()
