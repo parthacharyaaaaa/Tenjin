a50bbe5f7b74e78f13f52446606bba7b69cdc410
Parth Acharya <1008parth@gmail.com>
2025-06-01 15:21:41 +0530

    Moves counter updation/set logic to separate function

--- resource_server/resource_auxillary.py
+++ resource_server/resource_auxillary.py
@@ -8,30 +8,32 @@ from flask import Flask
 import time
 from traceback import format_exc
 import threading
-import os
+from typing import Literal
+from sqlalchemy import select, text
+from flask_sqlalchemy import SQLAlchemy
 
 EMAIL_REGEX = r"^(?=.{1,320}$)([a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]{1,64})@([a-zA-Z0-9.-]{1,255}\.[a-zA-Z]{2,16})$"     # RFC approved babyyyyy
 
-def poll_global_key_mapping(RedisInterface: Redis) -> dict[str, bytes]:
+def poll_global_key_mapping(interface: Redis) -> dict[str, bytes]:
     '''Poll "JWKS_MAPPING" hashmap in Redis for new key mapping'''
-    res: dict[str, str] = RedisInterface.hgetall('JWKS_MAPPING')
+    res: dict[str, str] = interface.hgetall('JWKS_MAPPING')
     if not res:
         raise RuntimeError('JWKS Mapping not found/ found but empty')
     
-    return {kid : pub_pem.encode() for kid, pub_pem in res.items()}  # RedisInterface has decoded responses, but PyJWT needs public pem in bytes
+    return {kid : pub_pem.encode() for kid, pub_pem in res.items()}  # interface has decoded responses, but PyJWT needs public pem in bytes
 
 
-def update_jwks(endpoint: str, currentMapping: dict[str, bytes], RedisInterface: Redis, current_app: Flask, timeout: int = 3, max_global_mapping_polls: int = 10) -> dict[str, str|int]:
+def update_jwks(endpoint: str, currentMapping: dict[str, bytes], interface: Redis, current_app: Flask, timeout: int = 3, max_global_mapping_polls: int = 10) -> dict[str, str|int]:
     '''Fetch JWKS from auth server and load any new key mappings into currentMapping'''
-    res: int = RedisInterface.set('JWKS_POLL_LOCK', 1, ex=current_app.config['ANNOUNCEMENT_DURATION'], nx=True)
+    res: int = interface.set('JWKS_POLL_LOCK', 1, ex=current_app.config['ANNOUNCEMENT_DURATION'], nx=True)
     if not res:
         # Another worker is currently polling JWKS, wait until lock is released and then read global key mapping
-        while(RedisInterface.get('JWKS_POLL_LOCK') and max_global_mapping_polls):
+        while(interface.get('JWKS_POLL_LOCK') and max_global_mapping_polls):
             print(f'[JWKS POLLER] Standing by for master thread to perform updation')
             time.sleep(timeout*2)
             max_global_mapping_polls-=1 # Ideally, the lock would always be released no matter what, but a fallback to stop the thread from waiting forever wouldn't hurt
         
-        global_mapping: dict[str, str] = poll_global_key_mapping(RedisInterface=RedisInterface) 
+        global_mapping: dict[str, str] = poll_global_key_mapping(interface=interface) 
         
         return {kid:pub_pem.encode() for kid, pub_pem in global_mapping}
     
@@ -66,7 +68,7 @@ def update_jwks(endpoint: str, currentMapping: dict[str, bytes], RedisInterface:
                 currentMapping[keyMetadata['kid']] = vk.to_pem()
 
         # Update global list and values in Redis to inform other workers
-        with RedisInterface.pipeline() as pipe:
+        with interface.pipeline() as pipe:
             # Overwrite mapping entirely
             pipe.delete('JWKS_MAPPING')
             pipe.hset('JWKS_MAPPING', mapping=currentMapping)
@@ -78,21 +80,21 @@ def update_jwks(endpoint: str, currentMapping: dict[str, bytes], RedisInterface:
 
         return currentMapping
     except Exception:
-        with RedisInterface.pipeline() as pipe:
+        with interface.pipeline() as pipe:
             pipe.delete('JWKS_POLL_LOCK')
             pipe.set('JWKS_POLL_COOLDOWN', current_app.config['JWKS_POLL_COOLDOWN'])
             pipe.execute()
         print(format_exc())
         return currentMapping
 
-def background_poll(current_app: Flask, RedisInterface: Redis, interval: int = 300):
+def background_poll(current_app: Flask, interface: Redis, interval: int = 300):
     def run():
         while True:
             try:
                 update_jwks(endpoint=f'{current_app.config["AUTH_SERVER_URL"]}/auth/jwks.json',
                             currentMapping= current_app.config['KEY_VK_MAPPING'],
                             current_app=current_app,
-                            RedisInterface=RedisInterface)
+                            interface=interface)
             except Exception:
                 print(f"[JWKS POLLER]: Error: {format_exc()}")
             finally:
@@ -138,3 +140,34 @@ def processUserInfo(**kwargs) -> tuple[bool, dict]:
         print(format_exc())
         return False, {"error" : "Malformatted data, please validate data types of each field"}
   
+def update_global_counter(interface: Redis, counter_name: str, delta: int, db: SQLAlchemy, table: str, column: str, identifier: int | str, counter_expiry: int = 86400, hashmap_key: str = None) -> None:
+    '''
+    Update the global counter
+    Args:
+        interface: Redis instance
+        counter_name: Name of the counter to be updated, in case an active counter is not found
+        delta: Whether to increment or decrement the counter
+        db: SQLAlchemy instance to fetch data from in case the counter is absent in Redis, and a new one needs to be made
+        table: Table name for the entity to be updated
+        column: Column of entity associated with the counter
+        identifier: Unique ID to identify the target record
+        counter_expiry: TTL for counter
+    '''
+    if not hashmap_key:
+        hashmap_key: str = f'{table}:{column}'
+    counter_key: str = interface.hget(hashmap_key, identifier)
+    if counter_key:
+        interface.incrby(counter_key, delta)
+        return 
+
+    # No counter, create one
+    currentCount: int = db.session.execute(text(f"SELECT {column} FROM {table} WHERE id = :identifier"), {'identifier':identifier}).scalar()
+    counter_key = counter_name
+    
+    op = interface.set(counter_key, currentCount+delta, nx=True, ex=counter_expiry)
+
+    if not op:
+        # Counter made by another worker
+        interface.incrby(counter_key, delta)
+    else:
+        interface.hset(hashmap_key, identifier, counter_key)
