86c3916a096d28fb326817154e4bde77d2e7500d
Parth Acharya <1008parth@gmail.com>
2025-06-05 16:02:01 +0530

    Adds batch_cache_write() and get_column_types()

--- resource_server/scripts/batch_workers/worker_utils.py
+++ resource_server/scripts/batch_workers/worker_utils.py
@@ -1,7 +1,9 @@
 '''Auxillary functions for batch workers'''
 import psycopg2 as pg
-from typing import Mapping
+from typing import Mapping, Callable, Any
 from types import MappingProxyType
+from redis import Redis
+from datetime import datetime
 
 # We got reinvented SQLAlchemy before GTA VI
 MAPPED_DTYPES: MappingProxyType[str, type] = MappingProxyType({'integer': int,
@@ -13,8 +15,8 @@ MAPPED_DTYPES: MappingProxyType[str, type] = MappingProxyType({'integer': int,
                                                                'character': str,
                                                                'text': str,  
                                                                'bytea': bytes,
-                                                               'timestamp without time zone': str,
-                                                               'timestamp with time zone': str,
+                                                               'timestamp without time zone': lambda dt : datetime.fromisoformat(dt),
+                                                               'timestamp with time zone': lambda dt : datetime.fromisoformat(dt),
                                                                'date': str,
                                                                'time without time zone': str,
                                                                'time with time zone': str,
@@ -60,6 +62,22 @@ def getDtypes(cursor: pg.extensions.cursor, table: str, includePrimaryKey: bool 
                        WHERE i.indisprimary AND t.relname = %s);''', (table, table))
     return [MAPPED_DTYPES.get(x[0], str) for x in cursor.fetchall()]
 
+def get_column_types(cursor: pg.extensions.cursor, table: str, includePrimaryKey: bool = False) -> list[type]:
+    '''Return ordered list of a table's column data types'''
+    if includePrimaryKey:
+        cursor.execute("SELECT data_type FROM information_schema.columns WHERE table_name = %s",(table,))
+    else:
+        cursor.execute('''SELECT c.data_type 
+                       FROM information_schema.columns c
+                       WHERE c.table_name = %s
+                       AND c.column_name NOT IN 
+                       (SELECT a.attname FROM pg_index i 
+                       JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
+                       JOIN pg_class t ON t.oid = i.indrelid
+                       WHERE i.indisprimary AND t.relname = %s);''', (table, table))
+    return [x[0] for x in cursor.fetchall()]
+
+
 def fetchDeletions(cursor: pg.extensions.cursor, table: str, castStr: bool = True):
     '''Fetch flagged rows from a given table, returing their primary key'''
     cursor.execute(f"SELECT id FROM {table} WHERE deleted = true FOR UPDATE SKIP LOCKED;")
@@ -68,3 +86,18 @@ def fetchDeletions(cursor: pg.extensions.cursor, table: str, castStr: bool = Tru
         return []
     
     return [str(pk[0]) for pk in result] if castStr else [pk[0] for pk in result]
+
+def batch_cache_write(interface: Redis, cache_entries: Mapping[str, Mapping[str, Any]], ttl: int, transaction: bool = False) -> None:
+    '''
+    Perform a batch write into cache with given mappings in a single network round trip
+    Args:
+        interface: Redis instance connected to cache server
+        cache_entries: Mapping of cache entries, where key is the name of the hashmap and correspoding key is the actual cache hashmap
+        ttl: TTL in seconds to assign to each cache entry
+        transaction: Whether to execute all cache writes atomically, Defaults to False to avoid overhead
+    '''
+    with interface.pipeline(transaction=transaction) as pipe:
+        for name, entry in cache_entries.items():
+            pipe.hset(name, mapping=entry)
+            pipe.expire(name, ttl)
+        pipe.execute()
