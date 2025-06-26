53f01cce6ffc5a829dde0e6c866f82863d611441
Parth Acharya <1008parth@gmail.com>
2025-06-26 17:40:43 +0530

    Removes unused endpoints, reused logic

--- auth_server/blueprint_routes.py
+++ auth_server/blueprint_routes.py
@@ -1,10 +1,9 @@
 from auth_server.token_manager import tokenManager
-from auxillary.decorators import enforce_json, private
-from flask import Blueprint, request, jsonify, Response, make_response, g, current_app, send_from_directory
+from auxillary.decorators import enforce_json
+from flask import Blueprint, request, jsonify, Response, g, current_app, send_from_directory, url_for
 from werkzeug.exceptions import BadRequest
 import requests
 import time
-import secrets
 from typing import Any
 from hashlib import sha256
 
@@ -24,21 +23,17 @@ def jwks() -> tuple[Response, int]:
     
 
 @auth.route("/login", methods = ["POST", "OPTIONS"])
-
 @enforce_json
 def login():
     if not ("identity" in g.REQUEST_JSON and "password" in g.REQUEST_JSON):
         raise BadRequest(f"POST /{request.root_path} expects identity and password in HTTP body")
-    for key in current_app.config["PRIVATE_COMM_KEYS"]:
-        valid = requests.post(f"{current_app.config['PROTOCOL']}://{current_app.config['RESOURCE_SERVER_ORIGIN']}{current_app.config['RESOURCE_SERVER_URL_PREFIX']}/users/login",
-                            json = {"identity" : g.REQUEST_JSON["identity"], "password" : g.REQUEST_JSON["password"]},
-                            headers={"PRIVATE-API-KEY" : key})
+    valid = requests.post(f"{current_app.config['PROTOCOL']}://{current_app.config['RESOURCE_SERVER_ORIGIN']}{current_app.config['RESOURCE_SERVER_URL_PREFIX']}/users/login",
+                        json = {"identity" : g.REQUEST_JSON["identity"], "password" : g.REQUEST_JSON["password"]})
+    
+    if valid.status_code != 200:
+        return jsonify({"message" : "Authentication Failed",
+                        "response_message" : valid.json().get("message", "None")}), valid.status_code
         
-        if valid.status_code != 200:
-            return jsonify({"message" : "Authentication Failed",
-                            "response_message" : valid.json().get("message", "None")}), valid.status_code
-        
-        break
     
     rsResponse: dict = valid.json()
     sub, sid = rsResponse.pop('sub'), rsResponse.pop('sid')
@@ -55,30 +50,10 @@ def login():
         "leeway" : tokenManager.leeway,
         "issuer" : "babel-auth-service"
     })
-    response.set_cookie(key="access",
-                        value=aToken,
-                        max_age=tokenManager.accessLifetime + tokenManager.leeway,
-                        httponly=True)
-    response.set_cookie(key="refresh",
-                        value=rToken,
-                        max_age=tokenManager.refreshLifetime + tokenManager.leeway,
-                        httponly=True,
-                        path="/auth/reissue")
-    response.set_cookie(key="refresh",
-                    value=rToken,
-                    max_age=tokenManager.refreshLifetime + tokenManager.leeway,
-                    httponly=True,
-                    path="/auth/delete-account")
-    response.set_cookie(key="refresh",
-                    value=rToken,
-                    max_age=tokenManager.refreshLifetime + tokenManager.leeway,
-                    httponly=True,
-                    path="/auth/purge-family")
+    tokenManager.attach_tokens_to_response(response, access_token=aToken, refresh_token=rToken, paths=[url_for('.reissue')])
     return response, 201
 
 @auth.route("/register", methods = ["POST", "OPTIONS"])
-
-
 @enforce_json
 def register():    
     if not ("username" in g.REQUEST_JSON and
@@ -91,15 +66,12 @@ def register():
         raise BadRequest("Passwords do not match")
     
     g.REQUEST_JSON.update({"authprovider" : "babel-auth"})
-    for key in current_app.config["PRIVATE_COMM_KEYS"]:
-        valid = requests.post(f"{current_app.config['PROTOCOL']}://{current_app.config['RESOURCE_SERVER_ORIGIN']}{current_app.config['RESOURCE_SERVER_URL_PREFIX']}/users/",
-                            json = g.REQUEST_JSON,
-                            headers={"PRIVATE-API-KEY" : key})
-        
-        if valid.status_code != 201:
-            return jsonify({"message" : "Failed to create account",
-                            "response_message" : valid.json().get("message", "Sowwy >:3")}), valid.status_code
-        break
+    valid = requests.post(f"{current_app.config['PROTOCOL']}://{current_app.config['RESOURCE_SERVER_ORIGIN']}{current_app.config['RESOURCE_SERVER_URL_PREFIX']}/users/",
+                        json = g.REQUEST_JSON)
+    
+    if valid.status_code != 201:
+        return jsonify({"message" : "Failed to create account",
+                        "response_message" : valid.json().get("message", "Sowwy >:3")}), valid.status_code
     
     rsResponse: dict[str, Any] = valid.json()
     sub, sid = rsResponse.pop('sub'), rsResponse.pop('sid')
@@ -107,9 +79,7 @@ def register():
     aToken = tokenManager.issueAccessToken(sub, sid, familyID)
     rToken = tokenManager.issueRefreshToken(sub, sid, familyID=familyID, reissuance=False)
     epoch = time.time()
-    response = jsonify({
-        "message" : rsResponse.pop("message", "Registration complete."),
-        "alias" : rsResponse.pop('alias', None),
+    response = jsonify({"message" : rsResponse.pop("message", "Registration complete."),
         "username" : sub,
         "email" : rsResponse.pop('email', None),
         "time_of_issuance" : epoch,
@@ -119,34 +89,8 @@ def register():
         "_additional" : {**rsResponse}
     })
 
-    response.set_cookie(key="access",
-                        value=aToken,
-                        max_age=tokenManager.accessLifetime + tokenManager.leeway,
-                        httponly=True)
-    response.set_cookie(key="refresh",
-                        value=rToken,
-                        max_age=tokenManager.refreshLifetime + tokenManager.leeway,
-                        httponly=True,
-                        path="/auth/reissue")
-    response.set_cookie(key="refresh",
-                        value=rToken,
-                        max_age=tokenManager.refreshLifetime + tokenManager.leeway,
-                        httponly=True,
-                        path="/auth/delete-account")
-    response.set_cookie(key="refresh",
-                    value=rToken,
-                    max_age=tokenManager.refreshLifetime + tokenManager.leeway,
-                    httponly=True,
-                    path="/auth/purge-family")
-
+    tokenManager.attach_tokens_to_response(response, access_token=aToken, refresh_token=rToken, paths=[url_for('.reissue')])
     return response, 201
-
-@auth.route("/delete-account", methods = ["DELETE"])
-@private
-def deleteAccount():
-    tokenManager.invalidateFamily(request.headers["refreshID"])
-    return jsonify({"message" : "resource deleted successfully"}), 204
-
 
 @auth.route("/reissue", methods = ["GET", "OPTIONS"])
 def reissue():
@@ -202,30 +146,6 @@ def purgeFamily():
                                    options={"verify_nbf" : False})
     if not tkn:
         raise BadRequest(f"Invalid Refresh Token provided to [{request.method}] {request.url_rule}")
-    tokenManager.invalidateFamily(tkn['fid'])
-    response : Response = jsonify({"message" : "Token Revoked"})
-    response.headers["iss"] = "babel-auth-service"
-    return response, 204
-
-
-@auth.route("/get-csrf", methods = ["OPTIONS", "GET"])
-def issueCSRF():
-    response = make_response()
-    response.headers["X-CSRF-TOKEN"] = secrets.token_urlsafe(32)
-    response.headers["TIME-OF-ISSUANCE"] = time.time()
-    response.headers["ISSUER"] = "babel-auth-service"
-
-    response.set_cookie(key="X-CSRF-TOKEN",
-                        value=response.headers["X-CSRF-TOKEN"],
-                        max_age=1200,
-                        httponly=True)
     
-    return response, 201
-
-@auth.route("/ip-blacklist", methods = ["POST"])
-def blacklist():
-    ...
-
-@auth.route("/get-blacklist", methods = ["GET"])
-def getBlacklist():
-    ...
+    tokenManager.invalidateFamily(tkn['fid'])
+    return jsonify({"message" : "Token Revoked"}), 200
