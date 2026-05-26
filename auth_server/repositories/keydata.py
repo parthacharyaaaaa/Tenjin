6f51f16393caa1bb5be55e193b6b22cb081affe2
Parth <1008parth@gmail.com>
2026-05-26 15:36:15 +0530

    Adds expire_keydata and insert_keydata methods

--- auth_server/repositories/keydata.py
+++ auth_server/repositories/keydata.py
@@ -1,8 +1,12 @@
 """Data access repository for Keydata SA model"""
 
 from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Literal, overload
 
-from sqlalchemy import select
+import ecdsa
+
+from sqlalchemy import insert, select, update
 from sqlalchemy.orm import Session, sessionmaker
 
 from auth_server.models.database import KeyData
@@ -39,3 +43,100 @@ class KeydataRepository(metaclass=SingletonMetaclass):
             if not keydata and raise_on_empty:
                 raise ValueError("Key data empty")
             return keydata
+
+    @overload
+    def insert_keydata(
+        self,
+        key_id: str,
+        private_key: ecdsa.SigningKey,
+        public_key: ecdsa.VerifyingKey,
+        alg: str,
+        curve: ecdsa.curves.Curve,
+        epoch: datetime | None = None,
+        *,
+        returning: Literal[False],
+    ) -> None: ...
+
+    @overload
+    def insert_keydata(
+        self,
+        key_id: str,
+        private_key: ecdsa.SigningKey,
+        public_key: ecdsa.VerifyingKey,
+        alg: str,
+        curve: ecdsa.curves.Curve,
+        epoch: datetime | None = None,
+        *,
+        returning: Literal[True],
+    ) -> KeyData: ...
+
+    def insert_keydata(
+        self,
+        key_id: str,
+        private_key: ecdsa.SigningKey,
+        public_key: ecdsa.VerifyingKey,
+        alg: str,
+        curve: ecdsa.curves.Curve,
+        epoch: datetime | None = None,
+        *,
+        returning: bool = False,
+    ) -> KeyData | None:
+        with self.session_maker() as session:
+            keydata: KeyData = session.execute(
+                insert(KeyData)
+                .values(
+                    kid=key_id,
+                    alg=alg,
+                    curve=str(curve),
+                    epoch=epoch or datetime.now(),
+                    private_pem=private_key.to_pem(),
+                    public_pem=public_key.to_pem(),
+                )
+                .returning(KeyData)
+            ).scalar_one()
+
+            session.commit()
+
+            if returning:
+                return keydata
+
+    @overload
+    def expire_keydata(
+        self,
+        threshold: datetime,
+        expiry_time: datetime | None = None,
+        *,
+        return_expired: Literal[False],
+    ) -> None: ...
+    @overload
+    def expire_keydata(
+        self,
+        threshold: datetime,
+        expiry_time: datetime | None = None,
+        *,
+        return_expired: Literal[True],
+    ) -> list[KeyData]: ...
+
+    def expire_keydata(
+        self,
+        threshold: datetime,
+        expiry_time: datetime | None = None,
+        *,
+        return_expired: bool = False,
+    ) -> list[KeyData] | None:
+        with self.session_maker() as session:
+            expired_keys: list[KeyData] = list(
+                session.execute(
+                    update(KeyData)
+                    .where(KeyData.epoch < threshold)
+                    .values(expired_at=expiry_time or datetime.now())
+                    .returning(KeyData)
+                )
+                .scalars()
+                .all()
+            )
+
+            session.commit()
+
+            if return_expired:
+                return expired_keys
