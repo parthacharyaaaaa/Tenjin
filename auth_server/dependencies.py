2a49399a702d400fd4e97a76aa19eeaf8be8c6cd
Parth <1008parth@gmail.com>
2026-05-26 13:01:23 +0530

    Adds get_database_session_maker and get_database_session

--- auth_server/dependencies.py
+++ auth_server/dependencies.py
@@ -1,8 +1,11 @@
 import os
 from functools import lru_cache
-from typing import Final
+from typing import Final, Generator
 
 from redis import Redis
+
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import create_engine, Engine
 
 from auth_server.config import AppConfig
 
@@ -32,3 +35,33 @@ def get_token_store_client() -> Redis:
         password=os.environ["AUTH_WORKER_REDIS_PASSWORD"],
         **config.REDIS.TOKEN_STORE.to_constructor_kwargs(),
     )
+
+
+@lru_cache(maxsize=1)
+def get_database_session_maker() -> sessionmaker[Session]:
+    config: Final[AppConfig] = get_app_config()
+
+    URI: Final[str] = config.DATABASE.SQLALCHEMY.derive_sqlalchemy_uri(
+        username=os.environ["POSTGRES_USERNAME"],
+        password=os.environ["POSTGRES_PASSWORD"],
+        host=str(config.DATABASE.POSTGRES_HOST),
+        port=config.DATABASE.POSTGRES_PORT,
+        database=config.DATABASE.POSTGRES_DATABASE,
+    )
+
+    engine: Final[Engine] = create_engine(URI)
+
+    session_maker: Final[sessionmaker[Session]] = sessionmaker(
+        bind=engine, autocommit=False, autoflush=False
+    )
+
+    return session_maker
+
+
+def get_database_session() -> Generator[Session, None, None]:
+    session_maker: sessionmaker = get_database_session_maker()
+    session: Final[Session] = session_maker()
+    try:
+        yield session
+    finally:
+        session.close()
