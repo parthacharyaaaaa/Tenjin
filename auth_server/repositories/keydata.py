4b1bedfdd05352ad652a49668e8b3615569f048d
Parth <1008parth@gmail.com>
2026-05-26 14:21:15 +0530

    Adds repositories for data access

---
+++ auth_server/repositories/keydata.py
@@ -0,0 +1,23 @@
+"""Data access repository for Keydata SA model"""
+
+from dataclasses import dataclass, field
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session, sessionmaker
+
+from auth_server.models.database import KeyData
+from auth_server.dependencies import get_database_session_maker
+from auth_server.utils.singleton import SingletonMetaclass
+
+
+@dataclass(frozen=True, slots=True, init=False, weakref_slot=True)
+class KeydataRepository(metaclass=SingletonMetaclass):
+    session_maker: sessionmaker[Session] = field(
+        default_factory=get_database_session_maker, init=False
+    )
+
+    def get_keydata(self, key_id: str) -> KeyData | None:
+        with self.session_maker() as session:
+            return session.execute(
+                select(KeyData).where(KeyData.kid == key_id)
+            ).scalar_one_or_none()
