d90c8f10d045af5c69e3b0186ce628dbc2e4af23
Parth <1008parth@gmail.com>
2026-05-27 17:58:42 +0530

    Updates SingletonMetaclass to be more type-friendly

--- auth_server/src/auth_server/utils/singleton.py
+++ auth_server/src/auth_server/utils/singleton.py
@@ -1,18 +1,26 @@
 import weakref
-from typing import Any
+from typing import Any, TypeVar
 
 __all__ = ("SingletonMetaclass",)
+
+T = TypeVar("T")
+
+# NOTE: Use of getattr and setattr is to avoid
+# type checker's complaining about reportAttributeAccessIssue
 
 
 class SingletonMetaclass(type):
     _instance: weakref.ReferenceType[Any] | None = None
 
-    def __call__(cls, *args: Any, **kwds: Any) -> Any:
-        if cls._instance:
-            return cls._instance()
+    def __call__(cls: type[T], *args: Any, **kwds: Any) -> T:
+        if attr := getattr(cls, "_instance", None):
+            return attr()
 
-        temp_instance: Any = super().__call__(*args, **kwds)
-        cls._instance = weakref.ref(
-            temp_instance, lambda _: setattr(cls, "_instance", None)
+        temp_instance: T = super().__call__(*args, **kwds)
+        setattr(
+            cls,
+            "_instance",
+            weakref.ref(temp_instance, lambda _: setattr(cls, "_instance", None)),
         )
-        return cls._instance()
+
+        return getattr(cls, "_instance")
