5bbe88a688a37ed87dcd9b2fb4ffef059af32f29
Parth <1008parth@gmail.com>
2026-05-29 20:52:54 +0530

    Makes I/O utility functions async

--- auth_server/src/auth_server/utils/auth_auxillary.py
+++ auth_server/src/auth_server/utils/auth_auxillary.py
@@ -3,9 +3,8 @@ from typing import Sequence
 from redis import Redis
 
 from sqlalchemy import func, select, insert, update
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio.session import AsyncSession
 from datetime import datetime, timedelta
-from flask import current_app
 from fastapi import Response
 from fastapi.datastructures import URL
 
@@ -41,15 +40,15 @@ def attach_tokens(
         )
 
 
-def report_suspicious_activity(
-    session: Session,
+async def report_suspicious_activity(
+    session: AsyncSession,
     config: AppConfig,
     synced_store_client: Redis,
     adminID: int,
     desc: str,
     force_logout: bool = True,
 ) -> None:
-    session.execute(
+    await session.execute(
         insert(SuspiciousActivity).values(suspect=adminID, description=desc)
     )
     current_time: datetime = datetime.now()
@@ -71,18 +70,19 @@ def report_suspicious_activity(
 
     if (
         force_logout
-        and session.execute(stmt).scalar() >= current_app.config["MAX_ACTIVITY_LIMIT"]
+        and (await session.execute(stmt)).scalar_one()
+        >= config.ADMIN.MAX_ACTIVITY_LIMIT
     ):
-        session.execute(update(Admin).values(locked=True))
+        await session.execute(update(Admin).values(locked=True))
         synced_store_client.delete(f"admin:{adminID}")
 
-    session.commit()
+    await session.commit()
 
 
-def fetch_valid_keys(session: Session) -> list[str]:
+async def fetch_valid_keys(session: AsyncSession) -> list[str]:
     """Fetch all valid key IDs from database"""
     valid_keys: list[str] = list(
-        session.execute(select(KeyData.kid).where(KeyData.expired_at.is_(None)))
+        (await session.execute(select(KeyData.kid).where(KeyData.expired_at.is_(None))))
         .scalars()
         .all()
     )
