8e5859d9aec1ad70ca9094341e126abcb90ebd4d
Parth <1008parth@gmail.com>
2026-05-29 22:10:29 +0530

    Adds REVIVAL_DIGEST_LENGTH

--- auth_server/src/auth_server/config/constants.py
+++ auth_server/src/auth_server/config/constants.py
@@ -13,3 +13,6 @@ MAX_EMAIL_LENGTH: Final[int] = 254
 # 254 byte length email addresses
 # (RFC 3696: https://www.rfc-editor.org/rfc/rfc3696#page-5,
 # errata: https://www.rfc-editor.org/errata_search.php?rfc=3696)
+
+# Admin Sessions
+REVIVAL_DIGEST_LENGTH: Final[int] = 256
