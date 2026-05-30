2837d3cb93786e4d7e17062b56478b84d931c8fc
Parth <1008parth@gmail.com>
2026-05-30 19:46:41 +0530

    Adds AdminStrings enum

--- auth_server/src/auth_server/strings.py
+++ auth_server/src/auth_server/strings.py
@@ -1,7 +1,11 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class SyncedStoreStrings(str, Enum):
+class AdminStrings(StrEnum):
+    SESSION_TOKEN_HEADER = "X-SESSION-TOKEN"
+
+
+class SyncedStoreStrings(StrEnum):
     ABORT = "ABORT"
     AUTH_BOOTUP_MASTER = "AUTH_BOOTUP_MASTER"
     VALID_KEYS = "VALID_KEYS"
