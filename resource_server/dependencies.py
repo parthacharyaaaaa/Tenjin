4a326656f849feefe17fbe778ca8ba59f0fc7b42
Parth <1008parth@gmail.com>
2026-06-01 16:38:00 +0530

    Adds DI function for Redis

--- resource_server/dependencies.py
+++ resource_server/dependencies.py
@@ -1,8 +1,25 @@
+import os
 from functools import lru_cache
+from typing import Final
 
 from resource_server.config.app_config import AppConfig
+
+from redis import Redis
 
 
 @lru_cache(maxsize=1)
 def get_app_config() -> AppConfig:
     return AppConfig()  # type: ignore[reportCallIssue]
+
+
+@lru_cache(maxsize=1)
+def get_redis_client() -> Redis:
+    config: Final[AppConfig] = get_app_config()
+
+    return Redis(
+        host=str(config.REDIS.HOST),
+        port=config.REDIS.PORT,
+        db=config.REDIS.DB,
+        username=os.environ["RESOURCE_SERVER_REDIS_USERNAME"],
+        password=os.environ["RESOURCE_SERVER_REDIS_PASSWORD"],
+    )
