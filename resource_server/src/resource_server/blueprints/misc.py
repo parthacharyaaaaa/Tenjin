afdbc4020e3909e6652c0cfef758daabe3ea51e2
Parth <1008parth@gmail.com>
2026-06-03 19:04:17 +0530

    Refactors misc blueprint with FastAPI

---
+++ resource_server/src/resource_server/blueprints/misc.py
@@ -0,0 +1,46 @@
+from typing import Annotated, Final
+from datetime import datetime
+
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+
+from redis.asyncio import Redis
+
+from resource_auxillary.strings import StreamName
+
+from resource_server.dependencies import get_genres, get_app_redis_client
+from resource_server.models.database import UserTicket
+from resource_server.models.requests import UserTicketModel
+
+MISC: Final[APIRouter] = APIRouter()
+
+
+@MISC.get("/genres")
+async def get_anime_genres() -> JSONResponse:
+    return JSONResponse(await get_genres())
+
+
+@MISC.post("/tickets")
+async def issue_ticket(
+    request_model: UserTicketModel,
+    redis_client: Annotated[Redis, Depends(get_app_redis_client)],
+) -> JSONResponse:
+    time_raised_iso: str = datetime.now().isoformat()
+    await redis_client.xadd(
+        StreamName.INSERTIONS.value,
+        fields={
+            "email": request_model.email,
+            "time_raised": time_raised_iso,
+            "description": request_model.description,
+            "table": UserTicket.__tablename__,
+        },
+    )
+
+    return JSONResponse(
+        {
+            "message": "Your report has been recorded",
+            "email": request_model.email,
+            "time": time_raised_iso,
+        },
+        202,
+    )
