4cde6b94e6b4acd25c42117a7c8f8b46550f74b4
Parth <1008parth@gmail.com>
2026-06-07 16:17:45 +0530

    Adds AbstractResult

---
+++ resource_server/src/resource_server/repositories/result_protocol.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import Any, Mapping, Self
+
+from redis.typing import FieldT, EncodableT
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class AbstractResult(ABC):
+    @classmethod
+    @abstractmethod
+    def construct_from_cache(cls, mapping: Mapping[str, Any]) -> Self: ...
+    @classmethod
+    @abstractmethod
+    def construct_from_orm(cls, obj: DeclarativeBase, *args, **kwargs) -> Self: ...
+
+    @abstractmethod
+    def __json_repr__(self) -> dict[str, Any]: ...
+    @abstractmethod
+    def __cache_repr__(self) -> dict[FieldT, EncodableT]: ...
