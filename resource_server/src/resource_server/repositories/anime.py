7788d76c474f08ed0427ea6e35578ff09ce6d410
Parth <1008parth@gmail.com>
2026-06-07 17:28:10 +0530

    Updates AnimeRepository and AnimeResult

--- resource_server/src/resource_server/repositories/anime.py
+++ resource_server/src/resource_server/repositories/anime.py
@@ -1,115 +1,109 @@
-import asyncio
 from dataclasses import dataclass
-from typing import Any, Final
+from typing import Any, Mapping, Self
 
 import orjson
+
+from redis.typing import FieldT, EncodableT
+
 from sqlalchemy import Row, select
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
 
-from resource_server.cache_manager import CacheManager
 from resource_server.models.database import (
     Anime,
     AnimeSubscription,
     StreamLink,
     Genre,
     AnimeGenre,
-    User,
 )
 from resource_server.utils.singleton import SingletonMetaclass
+from resource_server.repositories.result_protocol import AbstractResult
 
 from auxillary.utils import rediserialize, json_repr
 
 
-@dataclass(slots=True, frozen=True)
-class AnimeResult:
-    anime: Anime
-    stream_links: list[StreamLink]
-    genres: list[Genre]
+@dataclass(slots=True, init=False)
+class AnimeResult(AbstractResult):
+    id_: int
+    title: str
+    rating: float | None
+    members: int
+    synopsis: str
+
+    genres: list[str]
+    stream_links: dict[str, str]
+
+    @classmethod
+    def construct_from_cache(cls, mapping: Mapping[str, Any]) -> Self:
+        instance = cls()
+        instance.id_ = mapping["id"]
+        instance.title = mapping["title"]
+        instance.rating = mapping["rating"]
+        instance.members = mapping["members"]
+        instance.synopsis = mapping["synopsis"]
+
+        instance.genres = orjson.loads(mapping["genres"])
+        instance.stream_links = orjson.loads(mapping["stream_links"])
+
+        return instance
+
+    @classmethod
+    def construct_from_orm(
+        cls,
+        obj: Anime,
+        genres: list[Genre],
+        stream_links: list[StreamLink],
+        *args,
+        **kwargs,
+    ) -> Self:
+        instance = cls()
+        instance.id_ = obj.id_
+        instance.title = obj.title
+        instance.rating = obj.rating
+        instance.members = obj.members
+        instance.synopsis = obj.synopsis
+
+        instance.genres = [g.name_ for g in genres]
+        instance.stream_links = {s.website: s.url for s in stream_links}
+
+        return instance
+
+    def __json_repr__(self) -> dict[str, Any]:
+        return {
+            "id": self.id_,
+            "title": self.title,
+            "rating": float(self.rating) if self.rating else None,
+            "members": self.members,
+            "synopsis": self.synopsis,
+        }
+
+    def __cache_repr__(self) -> dict[FieldT, EncodableT]:
+        return {
+            "id": self.id_,
+            "title": self.title,
+            "rating": float(self.rating) if self.rating else "",
+            "members": self.members,
+            "synopsis": self.synopsis,
+        }
 
 
 @dataclass(slots=True, weakref_slot=True)
 class AnimeRepository(metaclass=SingletonMetaclass):
     session_maker: async_sessionmaker[AsyncSession]
-    cache_manager: CacheManager
 
-    @staticmethod
-    def derive_cache_key(key: int | str) -> str:
-        return f"{Anime.__tablename__}:{key}"
-
-    @staticmethod
-    def construct_anime_from_cache(mapping: dict[str, str]) -> Anime:
-        casted_mapping = {
-            k: Anime.deserialization_mapping()[k](v) for k, v in mapping.items()
-        }
-        casted_mapping["id_"] = casted_mapping.pop("id")
-
-        return Anime(**casted_mapping)
-
-    @staticmethod
-    def construct_cache_mapping(
-        anime: Anime, genres: list[Genre], stream_links: list[StreamLink]
-    ) -> dict[str, Any]:
-        return rediserialize(json_repr(anime)) | {
-            "genres": [g.name_ for g in genres],
-            "stream_links": {link.website: link.url for link in stream_links},
-        }
-
-    async def check_anime_existence(self, anime_id: int) -> bool:
-        cache_key: Final[str] = self.derive_cache_key(anime_id)
-        cache_result: dict[str, Any] | None = await self.cache_manager.fetch_from_cache(
-            cache_key, dtype="string"
-        )
-
-        if cache_result:
-            if self.cache_manager.cache_config.NF_SENTINEL_KEY in cache_result:
-                return False
-            return True
-
+    async def get_anime(self, anime_id: int) -> AnimeResult | None:
         async with self.session_maker() as session:
             anime: Anime | None = (
                 await session.execute(select(Anime).where(Anime.id_ == anime_id))
             ).scalar_one_or_none()
-
-            if not anime:
-                return False
-
-            asyncio.create_task(self._update_cache_from_anime(anime))
-            return True
-
-    async def get_anime_data(
-        self, anime_id: int
-    ) -> tuple[Anime, list[str], dict[str, str]] | None:
-        cache_key: Final[str] = self.derive_cache_key(anime_id)
-        cache_result: dict[str, Any] | None = await self.cache_manager.fetch_from_cache(
-            cache_key, dtype="string"
-        )
-
-        if cache_result:
-            if self.cache_manager.cache_config.NF_SENTINEL_KEY in cache_result:
-                return None
-            genres = cache_result.pop("genres", None)
-            stream_links = cache_result.pop("stream_links", None)
-
-            return self.construct_anime_from_cache(cache_result), genres, stream_links
-
-        async with self.session_maker() as session:
-            anime: Anime | None = (
-                await session.execute(select(Anime).where(Anime.id_ == anime_id))
-            ).scalar_one_or_none()
-
             if not anime:
                 return None
 
             genres, stream_links = await self.get_anime_details(
-                anime.id_, session=session
+                anime_id, session=session
             )
-            asyncio.create_task(self.update_cache(anime, genres, stream_links))
 
-            return (
-                anime,
-                [g.name_ for g in genres],
-                {s.website: s.url for s in stream_links},
-            )
+            return AnimeResult.construct_from_orm(anime, genres, stream_links)
 
     async def get_anime_details(
         self, anime_id: int, *, session: AsyncSession | None = None
@@ -145,44 +139,15 @@ class AnimeRepository(metaclass=SingletonMetaclass):
             if local_only_session:
                 await session.close()
 
-    async def update_cache(
-        self,
-        anime: Anime,
-        genres: list[Genre],
-        stream_links: list[StreamLink],
-        *,
-        cache_key: str | None = None,
-    ) -> None:
-        cache_key = cache_key or self.derive_cache_key(anime.id_)
-        anime_mapping: dict[str, Any] = self.construct_cache_mapping(
-            anime, genres, stream_links
-        )
-
-        await self.cache_manager.redis_client.set(
-            cache_key, orjson.dumps(anime_mapping)
-        )
-
-    async def _update_cache_from_anime(self, anime: Anime) -> None:
-        genres, stream_links = await self.get_anime_details(anime.id_)
-        await self.update_cache(anime, genres, stream_links)
-
-    async def get_anime_subscription(
-        self, anime_id: int, user_id: int
-    ) -> tuple[Anime, AnimeSubscription | None] | None:
+    async def check_subscription(self, anime_id: int, user_id: int) -> bool:
         async with self.session_maker() as session:
-            res: Row[tuple[Anime, AnimeSubscription]] | None = (
+            subscription: AnimeSubscription | None = (
                 await session.execute(
-                    select(Anime, AnimeSubscription)
-                    .outerjoin(
-                        AnimeSubscription,
-                        (AnimeSubscription.anime_id == anime_id)
-                        & (AnimeSubscription.user_id == user_id),
+                    select(AnimeSubscription).where(
+                        (AnimeSubscription.user_id == user_id)
+                        & (AnimeSubscription.anime_id == anime_id)
                     )
-                    .where(Anime.id_ == anime_id)
                 )
-            ).first()
+            ).scalar_one_or_none()
 
-            if not res:
-                return None
-
-            return res[0], res[1]
+            return bool(subscription)
