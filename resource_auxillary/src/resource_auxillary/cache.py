e8b8b97e11660b6995d4c99adcd5c2778a124e09
Parth <1008parth@gmail.com>
2026-06-08 12:07:49 +0530

    Updates string utils

--- resource_auxillary/src/resource_auxillary/cache.py
+++ resource_auxillary/src/resource_auxillary/cache.py
@@ -1,26 +1,13 @@
 from resource_auxillary.strings import NAME_SEPERATOR, Action, IntentFlag
 
 
-def create_creation_intent_flag(
-    flagname: IntentFlag,
+def create_intent_flag(
     entity: str,
     action: str,
     user_identifier: str,
     resource_identifier: str,
 ) -> str:
-    return NAME_SEPERATOR.join(
-        (flagname, entity, action, user_identifier, resource_identifier)
-    )
-
-
-def derive_deletion_intent_flag(resource_name: str, identifier: str | int) -> str:
-    return NAME_SEPERATOR.join(
-        (
-            IntentFlag.RESOURCE_DELETION_PENDING_FLAG,
-            resource_name,
-            str(identifier),
-        )
-    )
+    return NAME_SEPERATOR.join((entity, action, user_identifier, resource_identifier))
 
 
 def derive_cache_key(resource_name: str, identifier: str | int) -> str:
