35e14068072e932bd1722fea074bd5ba4b069285
Parth <1008parth@gmail.com>
2026-06-10 13:49:48 +0530

    Adds get_forum_by_name and create_forum methods

--- resource_server/src/resource_server/repositories/forum.py
+++ resource_server/src/resource_server/repositories/forum.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from functools import lru_cache
 from typing import Any, ClassVar, Mapping, Self
 
-from sqlalchemy import ColumnElement, and_, select
+from sqlalchemy import ColumnElement, and_, insert, select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
+from instance.resource_models import AdminRoles
 from resource_server.utils.singleton import SingletonMetaclass
 from resource_server.repositories.result_protocol import AbstractResult
-from resource_server.models.database import Forum
+from resource_server.models.database import Forum, ForumAdmin
 
 from resource_auxillary.strings import NAME_SEPERATOR
 
@@ -84,17 +85,28 @@ class ForumRepository(metaclass=SingletonMetaclass):
 
             return ForumResult.construct_from_orm(forum)
 
+    async def get_forum_by_name(self, name: str) -> ForumResult | None:
+        async with self.session_maker() as session:
+            forum: Forum | None = (
+                await session.execute(select(Forum).where(Forum.name_ == name))
+            ).scalar_one_or_none()
+
+            if not forum:
+                return None
+
+            return ForumResult.construct_from_orm(forum)
+
     async def get_forums(
         self,
         cursor: int = 0,
         search_param: str | None = None,
-        parent_anime: int | None = None,
+        parent_anime_id: int | None = None,
     ) -> list[ForumResult]:
         where_clauses: list[ColumnElement] = [Forum.id_ > cursor]
         if search_param:
             where_clauses.append(Forum.name_.ilike(f"%{search_param}%"))
-        if parent_anime:
-            where_clauses.append(Forum.anime == parent_anime)
+        if parent_anime_id:
+            where_clauses.append(Forum.anime == parent_anime_id)
 
         async with self.session_maker() as session:
             forums: list[Forum] = list(
@@ -104,3 +116,36 @@ class ForumRepository(metaclass=SingletonMetaclass):
             )
 
             return [ForumResult.construct_from_orm(f) for f in forums]
+
+    async def create_forum(
+        self,
+        name: str,
+        description: str,
+        parent_anime_id: int,
+        creator_id: int,
+        creation_time: datetime | None = None,
+    ) -> ForumResult:
+        async with self.session_maker() as session:
+            forum: Forum = (
+                await session.execute(
+                    insert(Forum)
+                    .values(
+                        name_=name,
+                        desscription=description,
+                        anime=parent_anime_id,
+                        created_at=creation_time,
+                    )
+                    .returning(Forum)
+                )
+            ).scalar_one()
+
+            await session.flush()
+            await session.execute(
+                insert(ForumAdmin).values(
+                    forum_id=forum.id_, user_id=creator_id, role=AdminRoles.owner
+                )
+            )
+            await session.commit()
+            return ForumResult.construct_from_orm(forum)
+
+    async def get_forum_owner(self, forum_id: int): ...
