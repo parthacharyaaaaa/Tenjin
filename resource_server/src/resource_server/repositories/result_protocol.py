5b08216180c3d8b1581734394eba4adf85768529
Parth <1008parth@gmail.com>
2026-06-10 10:34:47 +0530

    Updates AbstractResult to be a dataclass with default implementations

--- resource_server/src/resource_server/repositories/result_protocol.py
+++ resource_server/src/resource_server/repositories/result_protocol.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, fields
 from functools import lru_cache
 from typing import Any, Mapping, Self
 
@@ -6,8 +7,13 @@ from redis.typing import FieldT, EncodableT
 
 from sqlalchemy.orm import DeclarativeBase
 
+from resource_auxillary.cache import CACHE_TYPE_MAPPING
 
+
+@dataclass(slots=True, init=False)
 class AbstractResult(ABC):
+
+    # TODO: Add default implementation for construct_from_cache
     @classmethod
     @abstractmethod
     def construct_from_cache(cls, mapping: Mapping[str, Any]) -> Self: ...
@@ -15,10 +21,21 @@ class AbstractResult(ABC):
     @abstractmethod
     def construct_from_orm(cls, obj: DeclarativeBase, *args, **kwargs) -> Self: ...
 
-    @abstractmethod
-    def __json_repr__(self) -> dict[str, Any]: ...
-    @abstractmethod
-    def __cache_repr__(self) -> dict[FieldT, EncodableT]: ...
+    def __json_repr__(self) -> dict[str, Any]:
+        return {
+            field.name.strip("_"): getattr(self, field.name)
+            for field in fields(self)
+            if not field.name.startswith("_")
+        }
+
+    def __cache_repr__(self) -> dict[FieldT, EncodableT]:
+        return {
+            field.name.strip("_"): CACHE_TYPE_MAPPING.get(field.type, lambda x: x)(
+                getattr(self, field.name)  # type: ignore
+            )
+            for field in fields(self)
+            if not field.name.startswith("_")
+        }
 
     @lru_cache(maxsize=1)
     @abstractmethod
