eef1097d11efd5e792fb18f2f0c69dbb19341493
Parth <1008parth@gmail.com>
2026-06-13 14:49:50 +0530

    Adds rtbf methods, add_user, and get_user_by_identity

--- resource_server/src/resource_server/repositories/user.py
+++ resource_server/src/resource_server/repositories/user.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import ClassVar
 
-from sqlalchemy import select
+from sqlalchemy import insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from resource_server.repositories.result_protocol import AbstractResult
@@ -36,6 +36,21 @@ class UserResult(AbstractResult):
 class UserRepository(metaclass=SingletonMetaclass):
     session_maker: async_sessionmaker[AsyncSession]
 
+    async def add_user(
+        self, username: str, email: str, password: bytes | bytearray
+    ) -> UserResult:
+        async with self.session_maker() as session:
+            user: User = (
+                await session.execute(
+                    insert(User).values(
+                        username=username, email=email, pw_hash=password
+                    )
+                )
+            ).scalar_one()
+            await session.commit()
+
+            return UserResult.construct_from_orm(user)
+
     async def get_user(self, user_id: int) -> UserResult | None:
         async with self.session_maker() as session:
             user: User | None = (
@@ -50,6 +65,22 @@ class UserRepository(metaclass=SingletonMetaclass):
                 return None
 
             return UserResult.construct_from_orm(user)
+
+    async def get_user_by_identity(self, username: str, email: str) -> list[UserResult]:
+        async with self.session_maker() as session:
+            users: list[User] = list(
+                (
+                    await session.execute(
+                        select(User).where(
+                            (User.username == username) | (User.email == email)
+                        )
+                    )
+                )
+                .scalars()
+                .all()
+            )
+
+            return [UserResult.construct_from_orm(user) for user in users]
 
     async def get_user_by_username(
         self,
@@ -68,3 +99,16 @@ class UserRepository(metaclass=SingletonMetaclass):
                 return None
 
             return UserResult.construct_from_orm(user)
+
+    async def get_rtbf(self, user_id: int) -> bool:
+        async with self.session_maker() as session:
+            return (
+                await session.execute(select(User.rtbf).where(User.id_ == user_id))
+            ).scalar_one()
+
+    async def update_rtbf(self, user_id: int, rtbf: bool) -> None:
+        async with self.session_maker() as session:
+            await session.execute(
+                update(User).where(User.id_ == user_id).values(rtbf=rtbf)
+            )
+            await session.commit()
