f05afa2a1d8e226fe718c96f64e835fe0f9c1209
Parth <1008parth@gmail.com>
2026-06-14 13:49:39 +0530

    Renames blueprints package to routers

---
+++ resource_server/src/resource_server/routers/__init__.py
@@ -0,0 +1,25 @@
+"""Package for holding all routers"""
+
+from typing import Final
+
+from fastapi import APIRouter
+
+from resource_server.routers.url_prefixes import URLPrefix
+from resource_server.routers.users import USERS
+from resource_server.routers.animes import ANIMES
+from resource_server.routers.forums import FORUMS
+from resource_server.routers.posts import POSTS
+from resource_server.routers.comments import COMMENTS
+from resource_server.routers.misc import MISC
+
+type t_route_prefixes = tuple[tuple[APIRouter, tuple[URLPrefix, ...]], ...]
+
+ROUTER_PREFIXES: Final[t_route_prefixes] = tuple(
+    (
+        (USERS, (URLPrefix.USERS,)),
+        (ANIMES, (URLPrefix.ANIMES,)),
+        (POSTS, (URLPrefix.POSTS,)),
+        (COMMENTS, (URLPrefix.POSTS, URLPrefix.COMMENTS)),
+        (MISC, ()),
+    )
+)
