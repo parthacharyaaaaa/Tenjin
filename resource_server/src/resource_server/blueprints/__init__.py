0b03a728bbc918240b55011bda1708cc3598078e
Parth <1008parth@gmail.com>
2026-06-14 13:49:05 +0530

    Adds router prefixes

--- resource_server/src/resource_server/blueprints/__init__.py
+++ resource_server/src/resource_server/blueprints/__init__.py
@@ -1,36 +1,25 @@
 """Package for holding all blueprints"""
 
-from types import MappingProxyType
 from typing import Final
 
-from flask import Blueprint
+from fastapi import APIRouter
 
-from .blueprint_animes import ANIMES_BLUEPRINT
-from .blueprint_comments import COMMENTS_BLUEPRINT
-from .blueprint_forum import FORUMS_BLUEPRINT
-from .blueprint_misc import MISC_BLUEPRINT
-from .blueprint_posts import POSTS_BLUEPRINT
-from .blueprint_user import USERS_BLUEPRINT
-from .url_prefixes import URLPrefixes
+from resource_server.blueprints.url_prefixes import URLPrefix
+from resource_server.blueprints.users import USERS
+from resource_server.blueprints.animes import ANIMES
+from resource_server.blueprints.forums import FORUMS
+from resource_server.blueprints.posts import POSTS
+from resource_server.blueprints.comments import COMMENTS
+from resource_server.blueprints.misc import MISC
 
-__all__ = (
-    "ANIMES_BLUEPRINT",
-    "COMMENTS_BLUEPRINT",
-    "FORUMS_BLUEPRINT",
-    "MISC_BLUEPRINT",
-    "POSTS_BLUEPRINT",
-    "USERS_BLUEPRINT",
-    "URLPrefixes",
-    "PREFIX_MAPPING",
-)
+type t_route_prefixes = tuple[tuple[APIRouter, tuple[URLPrefix, ...]], ...]
 
-PREFIX_MAPPING: Final[MappingProxyType[Blueprint, URLPrefixes]] = MappingProxyType(
-    {
-        USERS_BLUEPRINT: URLPrefixes.USERS,
-        COMMENTS_BLUEPRINT: URLPrefixes.COMMENTS,
-        FORUMS_BLUEPRINT: URLPrefixes.FORUNS,
-        MISC_BLUEPRINT: URLPrefixes.MISC,
-        POSTS_BLUEPRINT: URLPrefixes.POSTS,
-        ANIMES_BLUEPRINT: URLPrefixes.ANIMES,
-    }
+ROUTER_PREFIXES: Final[t_route_prefixes] = tuple(
+    (
+        (USERS, (URLPrefix.USERS,)),
+        (ANIMES, (URLPrefix.ANIMES,)),
+        (POSTS, (URLPrefix.POSTS,)),
+        (COMMENTS, (URLPrefix.POSTS, URLPrefix.COMMENTS)),
+        (MISC, ()),
+    )
 )
