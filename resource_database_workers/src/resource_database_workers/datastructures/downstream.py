2a384329c81830a3e982834774aacd07719c3c9a
Parth <1008parth@gmail.com>
2026-06-23 12:50:16 +0530

    Updates downstream data structures

--- resource_database_workers/src/resource_database_workers/datastructures/downstream.py
+++ resource_database_workers/src/resource_database_workers/datastructures/downstream.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-from typing import TypedDict
-from resource_auxillary.datastructures.database import StrongEntity
+from types import MappingProxyType
+from typing import Mapping, TypedDict
+
+from resource_auxillary.datastructures.database import (
+    StrongEntity,
+    ForeignKeyColumnLiteral,
+)
 
 
 class AnonymousDownstreamDeletionData(TypedDict):
-    foreign_key_column: str
-    orphan_table: str
+    foreign_key_column: ForeignKeyColumnLiteral
+    orphan_table: StrongEntity
 
 
 class DownstreamDeletionData(AnonymousDownstreamDeletionData):
@@ -14,33 +19,43 @@ class DownstreamDeletionData(AnonymousDownstreamDeletionData):
 
 
 FORUM_COMMENT_DOWNSTREAM_DELETION_DATA = AnonymousDownstreamDeletionData(
-    foreign_key_column="parent_forum", orphan_table=StrongEntity.COMMENT.value
+    foreign_key_column=ForeignKeyColumnLiteral.PARENT_FORUM,
+    orphan_table=StrongEntity.COMMENT,
 )
 
 FORUM_POST_DOWNSTREAM_DELETION_DATA = AnonymousDownstreamDeletionData(
-    foreign_key_column="forum_id", orphan_table=StrongEntity.POST.value
+    foreign_key_column=ForeignKeyColumnLiteral.PARENT_FORUM,
+    orphan_table=StrongEntity.POST,
 )
 
 USER_COMMENT_DOWNSTREAM_DELETION_DATA = AnonymousDownstreamDeletionData(
-    foreign_key_column="author_id", orphan_table=StrongEntity.COMMENT.value
+    foreign_key_column=ForeignKeyColumnLiteral.AUTHOR_ID,
+    orphan_table=StrongEntity.COMMENT,
 )
 
 USER_POST_DOWNSTREAM_DELETION_DATA = AnonymousDownstreamDeletionData(
-    foreign_key_column="author_id", orphan_table=StrongEntity.POST.value
+    foreign_key_column=ForeignKeyColumnLiteral.AUTHOR_ID, orphan_table=StrongEntity.POST
 )
 
 POST_COMMENT_DOWNSTREAM_DELETION_DATA = AnonymousDownstreamDeletionData(
-    foreign_key_column="parent_post", orphan_table=StrongEntity.COMMENT.value
+    foreign_key_column=ForeignKeyColumnLiteral.PARENT_POST,
+    orphan_table=StrongEntity.COMMENT,
 )
 
-foo: dict[StrongEntity, tuple[AnonymousDownstreamDeletionData, ...]] = {
-    StrongEntity.FORUM: (
-        FORUM_COMMENT_DOWNSTREAM_DELETION_DATA,
-        FORUM_POST_DOWNSTREAM_DELETION_DATA,
-    ),
-    StrongEntity.USER: (
-        USER_COMMENT_DOWNSTREAM_DELETION_DATA,
-        USER_POST_DOWNSTREAM_DELETION_DATA,
-    ),
-    StrongEntity.POST: (POST_COMMENT_DOWNSTREAM_DELETION_DATA,),
-}
+type t_downstream_deletion_mapping = Mapping[
+    StrongEntity, tuple[AnonymousDownstreamDeletionData, ...]
+]
+
+DownstreamDeletionMapping: t_downstream_deletion_mapping = MappingProxyType(
+    {
+        StrongEntity.FORUM: (
+            FORUM_COMMENT_DOWNSTREAM_DELETION_DATA,
+            FORUM_POST_DOWNSTREAM_DELETION_DATA,
+        ),
+        StrongEntity.USER: (
+            USER_COMMENT_DOWNSTREAM_DELETION_DATA,
+            USER_POST_DOWNSTREAM_DELETION_DATA,
+        ),
+        StrongEntity.POST: (POST_COMMENT_DOWNSTREAM_DELETION_DATA,),
+    }
+)
