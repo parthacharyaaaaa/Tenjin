b855a42bcb71a62c8bf9fde9e18a23548daf4791
Parth <1008parth@gmail.com>
2026-06-30 14:15:03 +0530

    Adds event to input and function mapping

--- resource_database_workers/src/resource_database_workers/datastructures/processors.py
+++ resource_database_workers/src/resource_database_workers/datastructures/processors.py
@@ -1,12 +1,39 @@
-from enum import StrEnum
+from typing import Final
+from types import MappingProxyType
 
+from resource_auxillary.strings import EventName
 
-class ProcessorName(StrEnum):
-    INSERTIONS = "INSERTIONS"
-    DELETIONS = "DELETIONS"
-    DOWNSTREAM_DELETIONS = "DOWNSTREAM_DELETIONS"
-    DOWNSTREAM_COUNTER_DECREMENTS = "DOWNSTREAM_COUNTER_DECREMENTS"
-    DLQ = "DLQ"
-    COUNTERS = "COUNTERS"
-    DLQ_COUNTERS = "DLQ_COUNTERS"
-    PENDING_RECYCLER = "PENDING_RECYCLER"
+from resource_database_workers.tasks.consumer import (
+    queue_insertion_consumer,
+    queue_deletion_consumer,
+    queue_downstream_decrement_consumer,
+    queue_downstream_deletion_consumer,
+)
+
+EVENT_WORKER_MAPPING: Final[MappingProxyType] = MappingProxyType(
+    {
+        # Strong entity deletions
+        EventName.POST_DELETE: queue_insertion_consumer,
+        EventName.COMMENT_DELETE: queue_insertion_consumer,
+        EventName.FORUM_DELETE: queue_insertion_consumer,
+        # Association entity upserts
+        EventName.POST_SAVE: queue_insertion_consumer,
+        EventName.POST_UNSAVE: queue_insertion_consumer,
+        EventName.POST_VOTE: queue_insertion_consumer,
+        EventName.POST_UNVOTE: queue_insertion_consumer,
+        EventName.COMMENT_VOTE: queue_insertion_consumer,
+        EventName.COMMENT_UNVOTE: queue_insertion_consumer,
+        EventName.FORUM_SUB: queue_insertion_consumer,
+        EventName.FORUM_UNSUB: queue_insertion_consumer,
+        EventName.ANIME_SUB: queue_insertion_consumer,
+        EventName.ANIME_UNSUB: queue_insertion_consumer,
+        # Downstream orphan deletions
+        EventName.ORPHANED_POST_DELETE: queue_downstream_deletion_consumer,
+        EventName.ORPHANED_COMMENT_DELETE: queue_downstream_deletion_consumer,
+        # Downstream counters
+        EventName.DOWNSTREAM_USER_POST_DECREMENT: queue_downstream_decrement_consumer,
+        EventName.DOWNSTREAM_USER_COMMENT_DECREMENT: queue_downstream_decrement_consumer,
+        EventName.DOWNSTREAM_FORUM_POST_DECREMENT: queue_downstream_decrement_consumer,
+        EventName.DOWNSTREAM_POST_COMMENT_DECREMENT: queue_downstream_decrement_consumer,
+    }
+)
---
+++ resource_database_workers/src/resource_database_workers/datastructures/worker_inputs.py
@@ -0,0 +1,201 @@
+import asyncio
+from dataclasses import dataclass, field
+from types import MappingProxyType
+from typing import Any, Final
+
+from psycopg_pool import AsyncConnectionPool
+
+from redis.asyncio import Redis
+
+from resource_auxillary.datastructures.database import StrongEntity
+from resource_auxillary.events import StreamedEvent
+from resource_auxillary.datastructures.database import GenericLiterals
+from resource_auxillary.strings import EventName, StreamName
+from resource_database_workers.config.config import AppConfig
+
+from resource_database_workers.dependencies import (
+    get_connection_pool,
+    get_config,
+    get_app_redis,
+    get_internal_redis,
+    get_queue_registry,
+    get_worker_settings,
+)
+from resource_database_workers.datastructures.queues import QueueRegistry
+from resource_database_workers.tasks.deletions import (
+    downstream_soft_delete_strong_entity,
+)
+from resource_database_workers.tasks.insertions import (
+    batch_association_insert_with_isolation,
+)
+from resource_database_workers.utils.typing import (
+    BatchDownstreamDeletionFunction,
+    BatchInsertionFunction,
+    t_action_literal,
+)
+
+QUEUE_REGISTRY: Final[QueueRegistry] = get_queue_registry()
+
+
+@dataclass(slots=True, kw_only=True)
+class BaseInput:
+    stream_name: StreamName
+    pool: AsyncConnectionPool = field(default_factory=get_connection_pool)
+    group_name: str = field(default=get_config().WORKER.CONSUMER_GROUP_NAME)
+    dead_letter_queue: asyncio.Queue[StreamedEvent] = field(
+        default=QUEUE_REGISTRY.dead_letter
+    )
+
+
+@dataclass(slots=True, kw_only=True)
+class UpstreamDeletionInput(BaseInput):
+    table: StrongEntity
+    queue: asyncio.Queue[tuple[StreamedEvent]]
+    identifier_column: str = field(default=GenericLiterals.ID.value)
+    config: AppConfig = field(default_factory=get_config)
+    redis: Redis = field(default_factory=get_app_redis)
+
+
+@dataclass(slots=True, kw_only=True)
+class AssociationInsertionInput(BaseInput):
+    queue: asyncio.Queue[tuple[StreamedEvent]]
+    action: t_action_literal
+    config: AppConfig = field(default_factory=get_config)
+    redis: Redis = field(default_factory=get_app_redis)
+    batch_function: BatchInsertionFunction = field(
+        default=batch_association_insert_with_isolation
+    )
+
+
+@dataclass(slots=True, kw_only=True)
+class DownstreamDeletionInput(BaseInput):
+    queue: asyncio.Queue[StreamedEvent]
+    batch_function: BatchDownstreamDeletionFunction = field(
+        default=downstream_soft_delete_strong_entity
+    )
+    config: AppConfig = field(default_factory=get_config)
+    redis: Redis = field(default_factory=get_internal_redis)
+
+
+@dataclass(slots=True, kw_only=True)
+class DownstreamCounterDecrementInput(BaseInput):
+    config: AppConfig = field(default_factory=get_config)
+    redis: Redis = field(default_factory=get_internal_redis)
+    queue: asyncio.Queue[StreamedEvent]
+
+
+UPSTREAM_POST_DELETION_INPUT: Final[UpstreamDeletionInput] = UpstreamDeletionInput(
+    table=StrongEntity.POST,
+    stream_name=StreamName.POSTS,
+    queue=QUEUE_REGISTRY.post_deletions,
+)
+
+UPSTREAM_COMMENT_DELETION_INPUT: Final[UpstreamDeletionInput] = UpstreamDeletionInput(
+    table=StrongEntity.COMMENT,
+    queue=QUEUE_REGISTRY.comment_deletions,
+    stream_name=StreamName.COMMENTS,
+)
+
+UPSTREAM_FORUM_DELETION_INPUT: Final[UpstreamDeletionInput] = UpstreamDeletionInput(
+    table=StrongEntity.FORUM,
+    queue=QUEUE_REGISTRY.forum_deletions,
+    stream_name=StreamName.FORUMS,
+)
+
+
+POST_SAVE_INPUT: Final[AssociationInsertionInput] = AssociationInsertionInput(
+    stream_name=StreamName.POSTS,
+    queue=QUEUE_REGISTRY.post_save_insertions,
+    action="save",
+)
+
+POST_VOTE_INPUT: Final[AssociationInsertionInput] = AssociationInsertionInput(
+    stream_name=StreamName.POSTS,
+    queue=QUEUE_REGISTRY.post_vote_insertions,
+    action="vote",
+)
+
+COMMENT_VOTE_INPUT: Final[AssociationInsertionInput] = AssociationInsertionInput(
+    stream_name=StreamName.COMMENTS,
+    queue=QUEUE_REGISTRY.comment_vote_insertions,
+    action="vote",
+)
+
+FORUM_SUB_INPUT: Final[AssociationInsertionInput] = AssociationInsertionInput(
+    stream_name=StreamName.FORUMS,
+    queue=QUEUE_REGISTRY.forum_subscription_insertions,
+    action="subscribe",
+)
+
+ANIME_SUB_INPUT: Final[AssociationInsertionInput] = AssociationInsertionInput(
+    stream_name=StreamName.ANIMES,
+    queue=QUEUE_REGISTRY.anime_subscription_insertions,
+    action="subscribe",
+)
+
+
+ORPHANED_POST_DELETE_INPUT: Final[DownstreamDeletionInput] = DownstreamDeletionInput(
+    stream_name=StreamName.DOWNSTREAM_DELETIONS, queue=QUEUE_REGISTRY.downstream_posts
+)
+
+ORPHANED_COMMENT_DELETE_INPUT: Final[DownstreamDeletionInput] = DownstreamDeletionInput(
+    stream_name=StreamName.DOWNSTREAM_DELETIONS,
+    queue=QUEUE_REGISTRY.downstream_comments,
+)
+
+
+DOWNSTREAM_FORUMS_POSTS_COUNTER_DECREMENT: Final[DownstreamCounterDecrementInput] = (
+    DownstreamCounterDecrementInput(
+        queue=QUEUE_REGISTRY.downstream_forums_posts_counters,
+        stream_name=StreamName.DOWNSTREAM_COUNTER_DECREMENTS,
+    )
+)
+
+DOWNSTREAM_USERS_POSTS_COUNTER_DECREMENT: Final[DownstreamCounterDecrementInput] = (
+    DownstreamCounterDecrementInput(
+        queue=QUEUE_REGISTRY.downstream_user_posts_counters,
+        stream_name=StreamName.DOWNSTREAM_COUNTER_DECREMENTS,
+    )
+)
+
+DOWNSTREAM_POSTS_COMMENTS_COUNTER_DECREMENT: Final[DownstreamCounterDecrementInput] = (
+    DownstreamCounterDecrementInput(
+        queue=QUEUE_REGISTRY.downstream_posts_comments_counters,
+        stream_name=StreamName.DOWNSTREAM_COUNTER_DECREMENTS,
+    )
+)
+
+DOWNSTREAM_USERS_COMMENTS_COUNTER_DECREMENT: Final[DownstreamCounterDecrementInput] = (
+    DownstreamCounterDecrementInput(
+        queue=QUEUE_REGISTRY.downstream_users_comments_counters,
+        stream_name=StreamName.DOWNSTREAM_COUNTER_DECREMENTS,
+    )
+)
+
+WORKER_INPUT_DATA_MAPPING: Final[MappingProxyType[EventName, Any]] = MappingProxyType(
+    {
+        # Strong entity deletions
+        EventName.POST_DELETE: UPSTREAM_COMMENT_DELETION_INPUT,
+        EventName.COMMENT_DELETE: UPSTREAM_COMMENT_DELETION_INPUT,
+        EventName.FORUM_DELETE: UPSTREAM_FORUM_DELETION_INPUT,
+        # Association entity upserts
+        EventName.POST_SAVE: POST_SAVE_INPUT,
+        EventName.POST_UNSAVE: POST_SAVE_INPUT,
+        EventName.POST_VOTE: POST_VOTE_INPUT,
+        EventName.POST_UNVOTE: POST_VOTE_INPUT,
+        EventName.COMMENT_VOTE: COMMENT_VOTE_INPUT,
+        EventName.COMMENT_UNVOTE: COMMENT_VOTE_INPUT,
+        EventName.FORUM_SUB: FORUM_SUB_INPUT,
+        EventName.FORUM_UNSUB: FORUM_SUB_INPUT,
+        EventName.ANIME_SUB: ANIME_SUB_INPUT,
+        EventName.ANIME_UNSUB: ANIME_SUB_INPUT,
+        # Downstream orphan deletions
+        EventName.ORPHANED_POST_DELETE: ORPHANED_POST_DELETE_INPUT,
+        EventName.ORPHANED_COMMENT_DELETE: ORPHANED_COMMENT_DELETE_INPUT,
+        # Downstream counters
+        EventName.DOWNSTREAM_USER_POST_DECREMENT: DOWNSTREAM_USERS_POSTS_COUNTER_DECREMENT,
+        EventName.DOWNSTREAM_USER_COMMENT_DECREMENT: DOWNSTREAM_USERS_COMMENTS_COUNTER_DECREMENT,
+        EventName.DOWNSTREAM_FORUM_POST_DECREMENT: DOWNSTREAM_FORUMS_POSTS_COUNTER_DECREMENT,
+        EventName.DOWNSTREAM_POST_COMMENT_DECREMENT: DOWNSTREAM_POSTS_COMMENTS_COUNTER_DECREMENT,
+    }
+)
