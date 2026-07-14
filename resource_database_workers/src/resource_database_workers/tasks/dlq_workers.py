5494b434b46c2a05857da7722793af15582ef799
Parth <1008parth@gmail.com>
2026-07-14 19:04:55 +0530

    Adds worker logic for failed side effects

--- resource_database_workers/src/resource_database_workers/tasks/dlq_workers.py
+++ resource_database_workers/src/resource_database_workers/tasks/dlq_workers.py
@@ -6,13 +6,22 @@ from psycopg.sql import Composed
 
 from auxillary.utils import json_repr
 
-from resource_auxillary.events import StreamedEvent
+from resource_auxillary.events import (
+    CacheUpdate,
+    CounterUpdate,
+    IntentUpdate,
+    StreamedEvent,
+)
+from resource_auxillary.datastructures.database import SideEffectType
 
 from resource_database_workers.datastructures.dead_counter_batch import DeadCounterBatch
 from resource_database_workers.utils.sql_templates import (
     format_dlq_insertion_sql,
     format_counters_dlq_insertion_sql,
+    format_failed_side_effects_sql,
 )
+
+# TODO: Add abstraction for deduplication and retry logic
 
 
 async def dlq_consumer(
@@ -40,3 +49,45 @@ async def counters_dlq_consumer(
                 (batch.table, batch.column, batch.failure_time, batch.counters),
             )
             await conn.commit()
+
+
+async def side_effects_dlq_consumer(
+    pool: AsyncConnectionPool, queue: asyncio.Queue[StreamedEvent]
+) -> None:
+    insertion_sql: Final[Composed] = format_failed_side_effects_sql()
+    while True:
+        failed_side_effects_parent_event: StreamedEvent = await queue.get()
+        side_effect_groups: tuple[
+            tuple[
+                SideEffectType, tuple[CounterUpdate | IntentUpdate | CacheUpdate, ...]
+            ],
+            ...,
+        ] = (
+            (
+                SideEffectType.CACHE_INVALIDATION,
+                failed_side_effects_parent_event.side_effects.cache_invalidations,
+            ),
+            (
+                SideEffectType.COUNTER_UPDATE,
+                failed_side_effects_parent_event.side_effects.counter_updates,
+            ),
+            (
+                SideEffectType.INTENT_INVALIDATION,
+                failed_side_effects_parent_event.side_effects.intent_updates,
+            ),
+        )
+        async with pool.connection() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.executemany(
+                    insertion_sql,
+                    (
+                        (
+                            failed_side_effects_parent_event.event_id,
+                            side_effect_type.value,
+                            json_repr(side_effect),
+                        )
+                        for (side_effect_type, side_effects) in side_effect_groups
+                        for side_effect in side_effects
+                    ),
+                )
+                await conn.commit()
