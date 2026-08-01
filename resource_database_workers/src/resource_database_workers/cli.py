0f444dccca498d78fbfc872dcf378e6d4ddb2cb4
Parth <1008parth@gmail.com>
2026-08-01 12:59:49 +0530

    Updates CLI

--- resource_database_workers/src/resource_database_workers/cli.py
+++ resource_database_workers/src/resource_database_workers/cli.py
@@ -1,5 +1,8 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 import os
+from typing import Iterable
+
+from resource_auxillary.strings import StreamName
 
 
 def _check_file_existence(arg: str) -> str:
@@ -18,9 +21,28 @@ def get_argument_parser() -> ArgumentParser:
     )
 
     arg_parser.add_argument(
+        "worker_type", help="type of worker", choices=("counter", "stream")
+    )
+
+    arg_parser.add_argument(
         "worker_config_filepath",
         help="TOML filepath for worker count config",
         type=_check_file_existence,
     )
 
+    arg_parser.add_argument(
+        "--stream", help="Name of stream, if worker is a stream worker", type=StreamName
+    )
+
     return arg_parser
+
+
+def parse_args(argparser: ArgumentParser, args: Iterable[str]) -> Namespace:
+    parsed_args: Namespace = argparser.parse_args(args)
+
+    if parsed_args.worker_type == "stream" and not parsed_args.stream:
+        raise ValueError("Missing stream name")
+    elif parsed_args.worker_type != "stream" and parsed_args.stream:
+        print("Ignoring irrelevant argument: ", parsed_args.stream)
+
+    return parsed_args
