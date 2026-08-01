375043ee66a379606459bf504f4de90e0a676240
Parth <1008parth@gmail.com>
2026-08-01 14:05:56 +0530

    Reworks entrypoint and bootup logic

--- resource_database_workers/src/resource_database_workers/bootup.py
+++ resource_database_workers/src/resource_database_workers/bootup.py
@@ -1,17 +1,8 @@
 import asyncio
-from functools import partial
-from typing import Any, Callable
+from typing import Callable
 
-from psycopg_pool import AsyncConnectionPool
-
-from redis.asyncio import Redis
-
-from resource_auxillary.strings import StreamName
-
-from resource_database_workers.config.config import AppConfig
-from resource_database_workers.config.worker_config import WorkerSettings
-from resource_database_workers.datastructures.queues import QueueRegistry
 from resource_database_workers.datastructures.worker_inputs import (
+    COUNTER_WORKER_INPUT,
     WORKER_INPUT_DATA_MAPPING,
     UpstreamDispatcherInput,
 )
@@ -21,85 +12,70 @@ from resource_database_workers.datastructures.processors import (
 from resource_database_workers.datastructures.streams import (
     STREAM_CONSUMER_MAPPING,
 )
-from resource_database_workers.tasks.dlq_workers import dlq_consumer
+from resource_database_workers.config.worker_config import (
+    CounterWorkersConfig,
+    StreamWorkersConfig,
+)
+from resource_database_workers.src.resource_database_workers.dependencies import (
+    get_queue_registry,
+)
+from resource_database_workers.src.resource_database_workers.utils.strings import (
+    generate_worker_name,
+)
+from resource_database_workers.src.resource_database_workers.utils.typing import (
+    GenericDataclass,
+)
 from resource_database_workers.tasks.counters import (
     batch_update_counters,
 )
+from resource_database_workers.tasks.counters import (
+    batch_update_counters,
+    batch_update_retry_counters,
+)
+
+
+async def counter_worker_wrapper(
+    counter_config: CounterWorkersConfig,
+) -> None:
+    async with asyncio.TaskGroup() as tg:
+        for _ in range(counter_config.WORKER_COUNT):
+            tg.create_task(
+                batch_update_counters(**COUNTER_WORKER_INPUT.__dataclass_fields__)
+            )
+        for _ in range(counter_config.RETRY_WORKER_COUNT):
+            tg.create_task(
+                batch_update_retry_counters(**COUNTER_WORKER_INPUT.__dataclass_fields__)
+            )
+
+
+async def stream_worker_wrapper(stream_config: StreamWorkersConfig) -> None:
+    async with asyncio.TaskGroup() as tg:
+        reader_callable: Callable = STREAM_CONSUMER_MAPPING[stream_config.STREAM]
+        for event, worker_count in stream_config.EVENT_WORKER_COUNT_MAPPING.items():
+            worker_callable: Callable = EVENT_WORKER_MAPPING[event]
+            worker_arguments: GenericDataclass = WORKER_INPUT_DATA_MAPPING[event]
+            for i in range(1, worker_count + 1):
+                tg.create_task(
+                    worker_callable(**worker_arguments.__dataclass_fields__),
+                    name=generate_worker_name(event, i),
+                )
+        queue_mapping = get_queue_registry().resolve_stream_reader_queue_mapping(
+            stream_config.STREAM
+        )
+        reader_task_input: UpstreamDispatcherInput = UpstreamDispatcherInput(
+            stream_name=stream_config.STREAM, queue_mapping=queue_mapping
+        )
+        for i in range(1, stream_config.READER_COUNT + 1):
+            tg.create_task(
+                reader_callable(**reader_task_input.__dataclass_fields__),
+                name=generate_worker_name(stream_config.STREAM, i, base_name="reader"),
+            )
 
 
 async def spawn_tasks(
-    app_config: AppConfig,
-    worker_settings: WorkerSettings,
-    app_redis: Redis,
-    internal_redis: Redis,
-    queue_registry: QueueRegistry,
-    pg_connection_pool: AsyncConnectionPool,
+    worker_config: CounterWorkersConfig | StreamWorkersConfig,
 ) -> None:
-    async with asyncio.TaskGroup() as tg:
-        # DLQ
-        for _ in range(worker_settings.DLQ.STANDARD):
-            tg.create_task(dlq_consumer(pg_connection_pool, queue_registry.dead_letter))
-        for _ in range(worker_settings.DLQ.COUNTERS):
-            tg.create_task(
-                counters_dlq_consumer(
-                    pg_connection_pool, queue_registry.counter_dead_letter
-                )
-            )
-
-        # Counters
-        for _ in range(worker_settings.COUNTERS.WORKERS):
-            tg.create_task(
-                batch_update_counters(
-                    app_config,
-                    pg_connection_pool,
-                    queue_registry.counter_dead_letter,
-                    internal_redis,
-                    app_redis,
-                )
-            )
-            tg.create_task(
-                retry_batch_update_counters(
-                    app_config,
-                    pg_connection_pool,
-                    queue_registry.counter_dead_letter,
-                    internal_redis,
-                    app_redis,
-                )
-            )
-
-        # Readers
-        for (
-            stream,
-            consumer_count,
-        ) in worker_settings.READER.reader_count_mapping.items():
-            consumer_task: Callable = STREAM_CONSUMER_MAPPING[stream]
-
-            if stream == StreamName.DOWNSTREAM_COUNTER_DECREMENTS:
-                queue_mapping = queue_registry.downstream_decrement_event_queue_mapping
-            elif stream == StreamName.DOWNSTREAM_DELETIONS:
-                queue_mapping = queue_registry.downstream_deletion_event_queue_apping
-            else:
-                queue_mapping = queue_registry.event_queue_mapping
-
-            consumer_task_input: UpstreamDispatcherInput = UpstreamDispatcherInput(
-                stream_name=stream, queue_mapping=queue_mapping
-            )
-
-            for _ in range(consumer_count):
-                tg.create_task(
-                    partial(consumer_task, consumer_task_input.__dataclass_fields__)()
-                )
-
-        # Workers
-        for event, worker_count in (
-            worker_settings.DOWNSTREAM_DECREMENT.worker_count_mapping
-            | worker_settings.DOWNSTREAM_DELETION.worker_count_mapping
-            | worker_settings.UPSTREAM.worker_count_mapping
-        ).items():
-            worker_task: Callable = EVENT_WORKER_MAPPING[event]
-            worker_input: Any = WORKER_INPUT_DATA_MAPPING[event]
-
-            for _ in range(worker_count):
-                tg.create_task(
-                    partial(worker_task, worker_input.__dataclass_fields__)()
-                )
+    if isinstance(worker_config, CounterWorkersConfig):
+        await counter_worker_wrapper(worker_config)
+    else:
+        await stream_worker_wrapper(worker_config)
