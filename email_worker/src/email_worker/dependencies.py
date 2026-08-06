0842745c2d170917b5912f287bc954b10f662657
Parth <1008parth@gmail.com>
2026-08-06 17:35:30 +0530

    Adds DB dependencies

--- email_worker/src/email_worker/dependencies.py
+++ email_worker/src/email_worker/dependencies.py
@@ -5,12 +5,15 @@ from typing import Final
 from aiosmtplib import SMTP
 from ssl import create_default_context, Purpose
 
+from psycopg_pool import AsyncConnectionPool
 from redis.asyncio import Redis
 
 from email_worker.config.email_config import EmailConfig
 from email_worker.config.redis_config import RedisConfig
+from email_worker.config.db_config import DatabaseConfig
 
 
+### Configurations ###
 @lru_cache(maxsize=1)
 def get_email_config() -> EmailConfig:
     return EmailConfig()  # type: ignore[reportCallIssue]
@@ -21,6 +24,12 @@ def get_redis_config() -> RedisConfig:
     return RedisConfig()  # type: ignore[reportCallIssue]
 
 
+@lru_cache(maxsize=1)
+def get_database_config() -> DatabaseConfig:
+    return DatabaseConfig()  # type: ignore[reportCallIssue]
+
+
+### Third-Party Clients ###
 @lru_cache(maxsize=1)
 def get_redis_client() -> Redis:
     redis_config: RedisConfig = get_redis_config()
@@ -44,3 +53,18 @@ async def get_fresh_smtp_client() -> SMTP:
     )
     await smtp_client.connect()
     return smtp_client
+
+
+### Database ###
+@lru_cache(maxsize=1)
+def get_connection_pool() -> AsyncConnectionPool:
+    db_config: DatabaseConfig = get_database_config()
+    uri: str = db_config.derive_sqlalchemy_uri(
+        os.environ["POSTGRES_USERNAME"],
+        os.environ["POSTGRES_PASSWORD"],
+    )
+
+    return AsyncConnectionPool(
+        conninfo=uri,
+        **config.DATABASE.emit_connection_pool_constructor_kwargs(),  # type: ignore
+    )
