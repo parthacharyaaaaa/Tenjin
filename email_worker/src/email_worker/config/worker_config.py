3d38cd7a34846b4a0a043ea838ce252625fa302b
Parth <1008parth@gmail.com>
2026-08-12 15:40:05 +0530

    Updates config

--- email_worker/src/email_worker/config/worker_config.py
+++ email_worker/src/email_worker/config/worker_config.py
@@ -1,23 +1,41 @@
-from typing import Annotated, Self
+import tomllib
+from typing import Annotated, Any, ClassVar, MutableMapping, Self
 
-from pydantic import Field, model_validator
+from pydantic import Field
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
     TomlConfigSettingsSource,
 )
+from resource_auxillary.strings import EventName
 
 
 class WorkerCountConfig(BaseSettings):
-    USER_DELETION_EMAIL: Annotated[int, Field(ge=0)]
-    USER_REGISTRATION_EMAIL: Annotated[int, Field(ge=0)]
-    USER_PASSWORD_RECOVERY_EMAIL: Annotated[int, Field(ge=0)]
+    READERS_KEY: ClassVar[str] = "READERS"
+    WORKERS_KEY: ClassVar[str] = "WORKERS"
 
-    @model_validator(mode="after")
-    def validate_worker_counts(self) -> Self:
-        if all(i == 0 for i in self.__dataclass_fields__.values()):
-            raise ValueError("No worker tasks specified")
-        return self
+    READER_COUNT: Annotated[int, Field(ge=1)]
+    EVENT_WORKER_COUNT_MAPPING: dict[EventName, Annotated[int, Field(ge=1)]] = {}
+
+    @staticmethod
+    def normalize_config_mapping(d: MutableMapping[str, int]) -> dict[EventName, int]:
+        return {EventName(event): count for event, count in d.items()}
+
+    @classmethod
+    def construct_from_toml(cls, toml_filepath: str) -> Self:
+        with open(toml_filepath, "r", encoding="utf-8") as toml_file:
+            config_mapping: dict[str, Any] = tomllib.loads(toml_file.read())
+            reader_count: int | None = config_mapping[cls.READERS_KEY].get("COUNT")
+            stream_mapping: dict[str, int] | None = config_mapping.get(cls.WORKERS_KEY)
+        if not reader_count:
+            raise KeyError("No reader count found for email workers")
+        if not stream_mapping:
+            raise KeyError("No worker count found for email workers")
+
+        return cls(
+            READER_COUNT=reader_count,
+            EVENT_WORKER_COUNT_MAPPING=cls.normalize_config_mapping(stream_mapping),
+        )
 
     @classmethod
     def settings_customise_sources(
