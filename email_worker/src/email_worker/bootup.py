7da82bcf0018bce09501995080069198c39c000e
Parth <1008parth@gmail.com>
2026-08-12 17:12:09 +0530

    Adds bootup sequence

--- email_worker/src/email_worker/bootup.py
+++ email_worker/src/email_worker/bootup.py
@@ -1,28 +1,33 @@
 import asyncio
 from functools import partial
-from typing import Mapping
+from typing import Any, Callable, Coroutine, Final, Mapping, MutableMapping
 
-from resource_auxillary.datastructures.status_indicator import StatusController, StatusProxy
+from resource_auxillary.datastructures.status_indicator import (
+    StatusController,
+    StatusProxy,
+)
+from email_worker.config.worker_config import WorkerCountConfig
+from email_worker.datastructures.queue_registry import QueueRegistry
+from email_worker.dependencies import get_queue_registry, get_email_config
+from email_worker.tasks.emailing import email_dispatcher
+from email_worker.tasks.stream_reading import upstream_dispatcher
+from email_worker.datastructures.worker_inputs import (
+    GeneralEmailInput,
+    UpstreamDispatcherInput,
+)
+from email_worker.datastructures.streams import STREAM_EVENT_MAPPING
 
-from email_worker.dependencies import get_email_config, get_redis_client
-from email_worker.tasks import email_dispatcher
+type _t_worker_task_callable = Callable[[], Coroutine[None, None, None]]
 
-def _prepare_email_dispatcher_task() -> partial:
-    return partial(
-        email_dispatcher,
-        get_email_config(),
-        get_redis_client(),
-        
-    )
 
 async def tasks_wrapper(
-    worker_count: int,
+    callable_details: Mapping[str, _t_worker_task_callable],
     graceful_shutdown_timeout: float,
     status_controller: StatusController,
 ) -> None:
     tasks: tuple[asyncio.Task[None], ...] = tuple(
         asyncio.create_task(worker_callable(), name=name)
-        for name, worker_callable in worker_callables.items()
+        for name, worker_callable in callable_details.items()
     )
     failed, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
     assert len(failed) == 1  # nosec
@@ -50,4 +55,49 @@ async def tasks_wrapper(
     )
     raise exception
 
-async def spawn_tasks(task_count: int) -> None:
+
+def _prepare_reader_task_callables(
+    worker_count_config: WorkerCountConfig,
+    status_proxy: StatusProxy,
+    tasks_mapping: MutableMapping[str, _t_worker_task_callable],
+) -> None:
+    input_fields: Final[dict[str, Any]] = UpstreamDispatcherInput(
+        status_proxy=status_proxy
+    ).__dataclass_fields__
+    for i in range(1, worker_count_config.READER_COUNT + 1):
+        tasks_mapping[f"reader_{i}"] = partial(
+            upstream_dispatcher, **input_fields.copy()
+        )
+
+
+def _prepare_worker_task_callables(
+    worker_count_config: WorkerCountConfig,
+    status_proxy: StatusProxy,
+    tasks_mapping: MutableMapping[str, _t_worker_task_callable],
+) -> None:
+    queue_registry: Final[QueueRegistry] = get_queue_registry()
+    for event, worker_count in worker_count_config.EVENT_WORKER_COUNT_MAPPING.items():
+        for i in range(1, worker_count + 1):
+            tasks_mapping[f"dispatcher_{i}"] = partial(
+                email_dispatcher,
+                **GeneralEmailInput(
+                    stream_name=STREAM_EVENT_MAPPING[event],
+                    events_queue=queue_registry.event_queue_mapping[event],
+                    status_proxy=status_proxy,
+                ).__dataclass_fields__,
+            )
+
+
+async def spawn_tasks(worker_count_config: WorkerCountConfig) -> None:
+    status_controller: Final[StatusController] = StatusController()
+    status_proxy: Final[StatusProxy] = StatusProxy(status_controller)
+    callable_details: dict[str, _t_worker_task_callable] = {}
+
+    _prepare_reader_task_callables(worker_count_config, status_proxy, callable_details)
+    _prepare_worker_task_callables(worker_count_config, status_proxy, callable_details)
+
+    await tasks_wrapper(
+        callable_details,
+        get_email_config().WORKER.GRACEFUL_SHUTDOWN_PERIOD,
+        status_controller,
+    )
--- email_worker/src/email_worker/datastructures/worker_inputs.py
+++ email_worker/src/email_worker/datastructures/worker_inputs.py
@@ -1,23 +1,27 @@
 import asyncio
 from dataclasses import dataclass, field
-from typing import Self
+from typing import Final, Mapping
+from uuid import uuid4
 
 from psycopg import AsyncConnection
 from psycopg_pool import AsyncConnectionPool
 from psycopg.rows import TupleRow
 
 from redis.asyncio import Redis
+from resource_auxillary.datastructures.status_indicator import StatusProxy
 from resource_auxillary.events import StreamedEvent
 from resource_auxillary.strings import EventName, StreamName
 
 from email_worker.config.email_config import EmailConfig
-from email_worker.datastructures.streams import STREAM_EVENT_MAPPING
 from email_worker.dependencies import (
     get_connection_pool,
     get_email_config,
     get_queue_registry,
     get_redis_client,
 )
+from email_worker.datastructures.queue_registry import QueueRegistry
+
+_QUEUE_REGISTRY: Final[QueueRegistry] = get_queue_registry()
 
 
 @dataclass(slots=True, kw_only=True)
@@ -31,12 +35,20 @@ class GeneralEmailInput:
     dlq_stream_name: StreamName = field(default=StreamName.USER_EMAILS)
     events_queue: asyncio.Queue[tuple[StreamedEvent, ...]]
     stream_name: StreamName
+    status_proxy: StatusProxy
 
-    @classmethod
-    def derive_worker_args(cls, event: EventName) -> Self:
-        internal_queue: asyncio.Queue[tuple[StreamedEvent, ...]] = (
-            get_queue_registry().event_queue_mapping[event]
-        )
-        stream: StreamName = STREAM_EVENT_MAPPING[event]
 
-        return cls(stream_name=stream, events_queue=internal_queue)
+@dataclass(slots=True, kw_only=True)
+class UpstreamDispatcherInput:
+    config: EmailConfig = field(default_factory=get_email_config)
+    redis: Redis = field(default_factory=get_redis_client)
+    queue_mapping: Mapping[
+        EventName,
+        asyncio.Queue[tuple[StreamedEvent, ...]] | asyncio.Queue[StreamedEvent],
+    ] = field(default=_QUEUE_REGISTRY.event_queue_mapping)
+    dead_letter_stream_name: StreamName = field(default=StreamName.DEAD_LETTER_QUEUE)
+    stream_name: StreamName = field(default=StreamName.USER_EMAILS)
+    read_history: bool = field(default=True)
+    consumer_name: str = field(default_factory=lambda: uuid4().hex)
+    group_name: str = field(default=get_email_config().WORKER.CONSUMER_GROUP_NAME)
+    status_proxy: StatusProxy
