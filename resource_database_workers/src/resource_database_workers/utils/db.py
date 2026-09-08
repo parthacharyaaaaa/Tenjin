f627f76da735f7059276883ad9cf76f25e6933f6
Parth <1008parth@gmail.com>
2026-09-08 18:51:26 +0530

    Adds get_side_effect_row to fetch record from side effect tables

---
+++ resource_database_workers/src/resource_database_workers/utils/db.py
@@ -0,0 +1,27 @@
+from resource_auxillary.datastructures.database import SideEffectsLiteral
+from typing import Any, TypeVar
+
+from psycopg.connection_async import AsyncConnection
+from psycopg.rows import dict_row
+
+from pydantic import BaseModel
+
+from resource_auxillary.datastructures.database import SideEffectsTables, EventLiteral
+from resource_auxillary.templates.sql import prepare_side_effects_read_sql
+
+T = TypeVar("T", bound=BaseModel)
+
+
+async def get_side_effect_row(
+    conn: AsyncConnection, side_effects_table: SideEffectsTables, payload_type: type[T]
+) -> tuple[int, dict[str, Any]]:
+    async with conn.cursor(row_factory=dict_row) as cursor:
+        while True:
+            await cursor.execute(prepare_side_effects_read_sql(side_effects_table))
+            result: dict[str, Any] | None = await cursor.fetchone()
+            if result:
+                break  # TODO: Add backoff periods
+        return (
+            result[EventLiteral.EVENT_ID_COLUMN_NAME],
+            result[SideEffectsLiteral.SIDE_EFFECTS_PAYLOAD],
+        )
