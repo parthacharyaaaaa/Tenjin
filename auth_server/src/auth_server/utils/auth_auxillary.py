7a06e1e3e0c89c82849a0dcedfe5fda9093a451c
Parth <1008parth@gmail.com>
2026-09-22 17:12:34 +0530

    Removes unused functions

--- auth_server/src/auth_server/utils/auth_auxillary.py
+++ auth_server/src/auth_server/utils/auth_auxillary.py
@@ -1,31 +1,17 @@
-import secrets
-import time
 from datetime import UTC, datetime, timedelta
-from typing import Final, Sequence
-from uuid import uuid4
+from typing import Sequence
 
 from auxillary.data_structures.uow import MultiRepositoryWorkCoordinator
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.asymmetric.types import PrivateKeyTypes
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from fastapi import Response
 from fastapi.datastructures import URL
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from auth_server.config.app_config import AppConfig
-from auth_server.models.database import (
-    KeyData,
-)
 from auth_server.repositories.admin import AdminRepository
 from auth_server.repositories.suspicious_activity import (
     SuspiciousActivityRepository,
     SuspiciousActivityResult,
 )
-from auth_server.security.admin_roles import AdminRole
 from auth_server.security.admin_sessions import AdminSessionManager
-from auth_server.utils.typing import AdminSessionDict
 
 
 def attach_tokens(
@@ -88,48 +74,3 @@ async def report_suspicious_activity(
                 await admin_session_manager.terminate_session_via_object(
                     existing_session
                 )
-
-
-# TODO: Swap this out with KeydataRepository/s equivalent method
-async def fetch_valid_keys(session: AsyncSession) -> list[str]:
-    """Fetch all valid key IDs from database"""
-    valid_keys: list[str] = list(
-        (await session.execute(select(KeyData.kid).where(KeyData.expired_at.is_(None))))
-        .scalars()
-        .all()
-    )
-    if not valid_keys:
-        raise RuntimeError("No valid keys found")
-    return valid_keys
-
-
-def create_admin_session(
-    admin_id: int,
-    session_length: float,
-    revival_digest_length: int,
-    role: AdminRole,
-    session_iteration: int = 1,
-    epoch_timestamp: float | None = None,
-) -> AdminSessionDict:
-    epoch_timestamp = epoch_timestamp or time.time()
-    return AdminSessionDict(
-        admin_id=admin_id,
-        session_id=uuid4().int,
-        revival_digest=secrets.token_hex(revival_digest_length),
-        epoch_timestamp=epoch_timestamp,
-        expiry_timestamp=epoch_timestamp + session_length,
-        session_iteration=session_iteration,
-        role=role.value,
-    )
-
-
-def sign_session(
-    session_token: bytes | bytearray, signing_pem: bytes | bytearray
-) -> bytes:
-    signiny_key: PrivateKeyTypes = load_pem_private_key(signing_pem, password=None)
-    if not isinstance(signiny_key, ec.EllipticCurvePrivateKey):
-        raise ValueError("Key not EC")
-
-    signature: Final[bytes] = signiny_key.sign(session_token, ec.ECDSA(hashes.SHA256()))
-
-    return b".".join((session_token, signature))
