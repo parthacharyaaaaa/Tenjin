86180cc2d18467e8a23dc552994c6912920208eb
Parth <1008parth@gmail.com>
2026-09-24 18:17:27 +0530

    Adds EnrichedJSONResponse class

--- auxillary/src/auxillary/data_structures/exceptions.py
+++ auxillary/src/auxillary/data_structures/exceptions.py
@@ -1,22 +1,9 @@
 from collections.abc import Mapping
-from http import HTTPMethod
-from typing import Annotated, Any
+from typing import Any
 
 from fastapi.exceptions import StarletteHTTPException
-from pydantic import BaseModel
-from pydantic.functional_validators import BeforeValidator
 
-_whitespace_stripped_string = Annotated[str, BeforeValidator(lambda x: x.strip())]
-
-
-class HypermediaResponse(BaseModel):
-    href: _whitespace_stripped_string
-    rel: _whitespace_stripped_string
-    type: HTTPMethod
-
-
-class HypermediaResponseSequence(BaseModel):
-    links: list[HypermediaResponse]
+from auxillary.data_structures.response import HypermediaResponseSequence
 
 
 class EnrichedHTTPException(StarletteHTTPException):
---
+++ auxillary/src/auxillary/data_structures/response.py
@@ -0,0 +1,56 @@
+from collections.abc import Mapping
+from http import HTTPMethod
+from typing import Annotated, Any, ClassVar
+
+import orjson
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from pydantic.functional_validators import BeforeValidator
+from starlette.background import BackgroundTask
+
+_whitespace_stripped_string = Annotated[str, BeforeValidator(lambda x: x.strip())]
+
+
+class HypermediaResponse(BaseModel):
+    href: _whitespace_stripped_string
+    rel: _whitespace_stripped_string
+    type: HTTPMethod
+
+
+class HypermediaResponseSequence(BaseModel):
+    links: list[HypermediaResponse]
+
+
+class EnrichedJSONResponse(JSONResponse):
+    __slots__ = ("hypermedia_data", "array_key", "hypermedia_links_key")
+    default_hypermedia_links_key: ClassVar[str] = "_links"
+    default_response_key: ClassVar[str] = "response"
+
+    def __init__(
+        self,
+        content: Any,
+        status_code: int = 200,
+        headers: Mapping[str, str] | None = None,
+        media_type: str | None = None,
+        background: BackgroundTask | None = None,
+        hypermedia_data: HypermediaResponseSequence | None = None,
+        *,
+        array_key: str | None = None,
+        hypermedia_links_key: str | None = None,
+    ) -> None:
+        self.hypermedia_data = hypermedia_data
+        self.array_key = array_key
+        self.hypermedia_links_key = hypermedia_links_key
+        super().__init__(content, status_code, headers, media_type, background)
+
+    def render(self, content: Any) -> bytes:
+        if not isinstance(content, dict):
+            content = {self.array_key or self.default_response_key: content}
+        if self.hypermedia_data:
+            content[self.hypermedia_links_key or self.default_hypermedia_links_key] = {
+                self.hypermedia_links_key: [
+                    i.model_dump() for i in self.hypermedia_data.links
+                ]
+            }
+
+        return orjson.dumps(content)
